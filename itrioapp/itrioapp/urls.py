8a021cb75cf9ef47ce2c7c6f5b66a3b090818d0c
MarioA <maestradaz3@gmail.com>
2023-04-21 11:38:40 -0500

    Varios

--- itrioapp/itrioapp/urls.py
+++ itrioapp/itrioapp/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
     path('usuarios/', include("users.urls")),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-]
+] 
