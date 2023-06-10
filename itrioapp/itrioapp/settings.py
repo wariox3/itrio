847b26da4041920f42cfab2b5676b53e6b4f64fc
sebas957 <baxtian957>
2023-06-10 10:02:12 -0500

    ajustes cors

--- itrioapp/itrioapp/settings.py
+++ itrioapp/itrioapp/settings.py
@@ -174,6 +174,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://179.15.105.37",
     "http://159.203.57.104",
     "http://muupservicios.online",
+    "http://*.muup.online",
     "http://muup.online",
     'http://localhost:4200',
 ]
