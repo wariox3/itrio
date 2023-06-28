5a6af3bc2a7a8e5ee9ab40801f3055113f97b9c6
sebas957 <baxtian957>
2023-06-28 15:31:37 -0500

    Se ajusta error de api empresa

--- itrioapp/inquilino/models.py
+++ itrioapp/inquilino/models.py
@@ -5,6 +5,7 @@ class Empresa(TenantMixin):
     schema_name = models.CharField(max_length=100)
     nombre = models.CharField(max_length=200, null=True)
     fecha = models.DateField(auto_now_add=True)
+    imagen = models.TextField(null=True)
     # default true, schema will be automatically created and synced when it is saved
     auto_create_schema = True
 
