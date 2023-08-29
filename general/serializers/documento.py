20e844249e1950b1d5567eb7bb353c72271b77e5
sebas957 <baxtian957>
2023-08-29 10:46:43 -0500

    ajustes doc

--- general/serializers/documento.py
+++ general/serializers/documento.py
@@ -1,31 +1,24 @@
 from rest_framework import serializers
 from general.models.documento import Documento
 from general.models.documento_tipo import DocumentoTipo
-from general.models.contacto import Contacto
-
-class ContactoSerializador(serializers.ModelSerializer):
-    class Meta:
-        model = Contacto
-        fields = ['nombre_corto', 'numero_identificacion']
-
-class DocumentoTipoSerializador(serializers.ModelSerializer):
-    class Meta:
-        model = DocumentoTipo
-        fields = ['nombre']
 
 class DocumentoSerializador(serializers.HyperlinkedModelSerializer):    
-    documentoTipo = DocumentoTipoSerializador()
-    contacto = ContactoSerializador()    
+    documento_tipo = serializers.PrimaryKeyRelatedField(queryset=DocumentoTipo.objects.all())    
     class Meta:
         model = Documento
-        fields = ['id', 'documento_tipo', 'subtotal', 'descuento', 'impuesto', 'total_bruto', 'total', 'fecha', 'contacto', 'numero', 'fecha_vence', 'contacto_id']
+        fields = ['id', 'documento_tipo', 'subtotal', 'descuento', 'impuesto', 'total_bruto', 'total', 'fecha', 'contacto', 'numero', 'fecha_vence']
 
     def to_representation(self, instance):
-        representation = super().to_representation(instance)  # Obtener la representación predeterminada del objeto
-
-        # Agregar los campos personalizados
-        representation['nombre_corto'] = instance.contacto.nombre_corto if instance.contacto else None
-        representation['numero_identificacion'] = instance.contacto.numero_identificacion if instance.contacto else None
-        representation['nombre'] = instance.documento_tipo.nombre if instance.documento_tipo else None
-        
-        return representation
+        return {
+            'id': instance.id,            
+            'documento_tipo_id': instance.documento_tipo_id,
+            'subtotal': instance.subtotal,
+            'descuento': instance.descuento,
+            'impuesto': instance.impuesto,
+            'total_bruto': instance.total_bruto,
+            'total' :  instance.total,
+            'fecha' : instance.fecha,
+            'fecha_vence' : instance.fecha_vence,
+            'contacto_id' : instance.contacto_id,
+            'numero' : instance.numero
+        }
