a3c476152b41950dbe8d026c297795fdb34048a8
sebas957 <baxtian957>
2023-08-29 10:24:15 -0500

    ajustes doc

--- general/serializers/documento.py
+++ general/serializers/documento.py
@@ -1,24 +1,16 @@
 from rest_framework import serializers
 from general.models.documento import Documento
 from general.models.documento_tipo import DocumentoTipo
+from general.models.contacto import Contacto
+
+class ContactoSerializador(serializers.ModelSerializer):
+    class Meta:
+        model = Contacto
+        fields = ['nombre_corto', 'numero_identificacion']
 
 class DocumentoSerializador(serializers.HyperlinkedModelSerializer):    
-    documento_tipo = serializers.PrimaryKeyRelatedField(queryset=DocumentoTipo.objects.all())    
+    documento_tipo = serializers.PrimaryKeyRelatedField(queryset=DocumentoTipo.objects.all())
+    contacto = ContactoSerializador()    
     class Meta:
         model = Documento
         fields = ['id', 'documento_tipo', 'subtotal', 'descuento', 'impuesto', 'total_bruto', 'total', 'fecha', 'contacto', 'numero', 'fecha_vence']
-
-    def to_representation(self, instance):
-        return {
-            'id': instance.id,            
-            'documento_tipo_id': instance.documento_tipo_id,
-            'subtotal': instance.subtotal,
-            'descuento': instance.descuento,
-            'impuesto': instance.impuesto,
-            'total_bruto': instance.total_bruto,
-            'total' :  instance.total,
-            'fecha' : instance.fecha,
-            'fecha_vence' : instance.fecha_vence,
-            'contacto_id' : instance.contacto_id,
-            'numero' : instance.numero
-        }
