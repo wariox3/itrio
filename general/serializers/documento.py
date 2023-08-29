3f06a83097c6a58bd09f0d6d5b117c429f507e51
sebas957 <baxtian957>
2023-08-29 09:52:35 -0500

    ajustes entidad documento

--- general/serializers/documento.py
+++ general/serializers/documento.py
@@ -6,7 +6,7 @@ class DocumentoSerializador(serializers.HyperlinkedModelSerializer):
     documento_tipo = serializers.PrimaryKeyRelatedField(queryset=DocumentoTipo.objects.all())    
     class Meta:
         model = Documento
-        fields = ['id', 'documento_tipo', 'subtotal', 'descuento', 'impuesto', 'total_bruto', 'total']
+        fields = ['id', 'documento_tipo', 'subtotal', 'descuento', 'impuesto', 'total_bruto', 'total', 'fecha', 'contacto', 'numero', 'fecha_vence']
 
     def to_representation(self, instance):
         return {
@@ -16,5 +16,9 @@ class DocumentoSerializador(serializers.HyperlinkedModelSerializer):
             'descuento': instance.descuento,
             'impuesto': instance.impuesto,
             'total_bruto': instance.total_bruto,
-            'total' :  instance.total
+            'total' :  instance.total,
+            'fecha' : instance.fecha,
+            'fecha_vence' : instance.fecha_vence,
+            'contacto' : instance.contacto,
+            'numero' : instance.numero
         }
