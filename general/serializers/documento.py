887bba98e3ceeeb9a5b0c51632e7d4506ed4ee56
sebas957 <baxtian957>
2023-08-29 10:45:48 -0500

    ajustes doc

--- general/serializers/documento.py
+++ general/serializers/documento.py
@@ -11,28 +11,21 @@ class ContactoSerializador(serializers.ModelSerializer):
 class DocumentoTipoSerializador(serializers.ModelSerializer):
     class Meta:
         model = DocumentoTipo
-        fields = ['documento_tipo_id', 'nombre']
+        fields = ['nombre']
 
 class DocumentoSerializador(serializers.HyperlinkedModelSerializer):    
     documentoTipo = DocumentoTipoSerializador()
     contacto = ContactoSerializador()    
     class Meta:
         model = Documento
-        fields = ['id', 'subtotal', 'descuento', 'impuesto', 'total_bruto', 'total', 'fecha', 'contacto', 'numero', 'fecha_vence']
+        fields = ['id', 'documento_tipo', 'subtotal', 'descuento', 'impuesto', 'total_bruto', 'total', 'fecha', 'contacto', 'numero', 'fecha_vence', 'contacto_id']
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)  # Obtener la representación predeterminada del objeto
 
-        # Obtener la representación de los objetos relacionados
-        documento_tipo_representation = representation.pop('documentoTipo')
-        contacto_representation = representation.pop('contacto')
-        
-        # Agregar los campos del serializador DocumentoTipoSerializador
-        for field, value in documento_tipo_representation.items():
-            representation[field] = value
-
-        # Agregar los campos del serializador ContactoSerializador
-        for field, value in contacto_representation.items():
-            representation[field] = value
+        # Agregar los campos personalizados
+        representation['nombre_corto'] = instance.contacto.nombre_corto if instance.contacto else None
+        representation['numero_identificacion'] = instance.contacto.numero_identificacion if instance.contacto else None
+        representation['nombre'] = instance.documento_tipo.nombre if instance.documento_tipo else None
         
         return representation
