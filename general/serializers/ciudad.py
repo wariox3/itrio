7d6718c5fad8dc6794679b321fbebc0346b17c55
sebas957 <baxtian957>
2023-09-18 09:10:37 -0500

    ajustes back

--- general/serializers/ciudad.py
+++ general/serializers/ciudad.py
@@ -26,7 +26,12 @@ class CiudadListaAutocompletarSerializador(serializers.HyperlinkedModelSerialize
         model = Ciudad
 
     def to_representation(self, instance):
+        nombre_ciudad = instance.nombre
+        nombre_estado = instance.estado.nombre
+
+        nombre_completo = f"{nombre_ciudad} - {nombre_estado}"
+
         return {
-            'ciudad_id': instance.id,            
-            'ciudad_nombre': instance.nombre,
-        }           
+            'id': instance.id,            
+            'nombre': nombre_completo,
+        }        
