f2f61cbef4e98be52e46cb491bc24ed1aa2be8e2
sebas957 <baxtian957>
2024-03-06 14:04:38 -0500

    ajustes CORS

--- itrioapp/settings.py
+++ itrioapp/settings.py
@@ -188,7 +188,7 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https?://(\w+\.)?muup\.online$",
-    r"^https?://(\w+\.)?reddoc\.online$",
+    r"^http?://(\w+\.)?reddoc\.online$",
     r"http?://(\w+\.)?localhost:4200"
     r"http?://(\w+\.)?localhost:8000"
 ]
