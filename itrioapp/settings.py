a0b432d954995dc9dd00691102b2fe06f7f7c849
sebas957 <baxtian957>
2024-03-06 14:07:06 -0500

    ajustes CORS

--- itrioapp/settings.py
+++ itrioapp/settings.py
@@ -172,6 +172,8 @@ AUTH_USER_MODEL = 'seguridad.User'
 CORS_ALLOWED_ORIGINS = [
     "https://example.com",
     "https://sub.example.com",
+    "http://example.com",
+    "http://sub.example.com",
     "http://localhost:8080",
     "http://127.0.0.1:9000",
     "http://127.0.0.1:8000",
