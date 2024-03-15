568b703aa94750f2c8a9b0c5c808031e60a37f5d
MarioA <maestradaz3@gmail.com>
2024-03-15 14:59:55 -0500

    Varios

--- contabilidad/models/cuenta.py
+++ contabilidad/models/cuenta.py
@@ -3,6 +3,9 @@ from django.db import models
 class Cuenta(models.Model):    
     codigo = models.IntegerField(null=True)
     nombre = models.CharField(max_length=100, null=True)
+    movimiento = models.BooleanField(default = False)
+    requiere_tercero = models.BooleanField(default = False)
+    requiere_centro_costo = models.BooleanField(default = False)
 
     class Meta:
         db_table = "con_cuenta"
