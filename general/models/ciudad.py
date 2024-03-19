72d33fd5c116b4a0cfed1d259cfc2ee8987f6670
MarioA <mestradaz3@gmail.com>
2024-03-19 10:06:23 -0500

    Varios

--- general/models/ciudad.py
+++ general/models/ciudad.py
@@ -8,6 +8,7 @@ class Ciudad(models.Model):
     longitud = models.DecimalField(max_digits=9, decimal_places=6, null=True)
     codigo_postal = models.CharField(max_length=10, null=True)
     porcentaje_impuesto = models.DecimalField(max_digits=5, decimal_places=2, default=0)  
+    codigo = models.CharField(max_length=10, null=True)
     estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
     
     class Meta:
