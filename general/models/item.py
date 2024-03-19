16919450291701b8f4a01caedf57088def3447f0
MarioA <mestradaz3@gmail.com>
2024-03-19 07:53:11 -0500

    Varios

--- general/models/item.py
+++ general/models/item.py
@@ -1,11 +1,16 @@
 from django.db import models
 
 class Item(models.Model):
-    nombre = models.CharField(max_length=200, verbose_name='Nombre')
-    codigo = models.CharField(max_length=100, null=True, verbose_name='Codigo')
-    referencia = models.CharField(max_length=50, null=True, verbose_name='Referencia')
-    costo = models.FloatField(default=0, verbose_name='Costo')
-    precio = models.FloatField(default=0, verbose_name='Precio')
+    nombre = models.CharField(max_length=200)
+    codigo = models.CharField(max_length=100, null=True)
+    referencia = models.CharField(max_length=50, null=True)
+    costo = models.FloatField(default=0)
+    precio = models.FloatField(default=0)
+    producto = models.BooleanField(default = False)
+    servicio = models.BooleanField(default = False)
+    inventario = models.BooleanField(default = False)
+    existencia = models.FloatField(default=0)
+    disponible = models.FloatField(default=0)
 
     class Meta:
         db_table = "gen_item"  
