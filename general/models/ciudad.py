b6c034a3c97446dc6ff486890ef523ce6ba52ef7
MarioA <mestradaz3@gmail.com>
2024-03-19 12:11:36 -0500

    Varios

--- general/models/ciudad.py
+++ general/models/ciudad.py
@@ -4,8 +4,8 @@ from general.models.estado import Estado
 class Ciudad(models.Model):
     id = models.BigIntegerField(primary_key=True)
     nombre = models.CharField(max_length=50) 
-    latitud = models.DecimalField(max_digits=9, decimal_places=9, null=True)
-    longitud = models.DecimalField(max_digits=9, decimal_places=9, null=True)
+    latitud = models.CharField(max_length=20, null=True)
+    longitud = models.CharField(max_length=20, null=True)
     codigo_postal = models.CharField(max_length=10, null=True)
     porcentaje_impuesto = models.DecimalField(max_digits=5, decimal_places=2, default=0)  
     codigo = models.CharField(max_length=10, null=True)
