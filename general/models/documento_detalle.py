48120ad6f377bb463dde2b7ae41874e2ef30b620
MarioA <mestradaz3@gmail.com>
2024-03-22 07:40:42 -0500

    Varios

--- general/models/documento_detalle.py
+++ general/models/documento_detalle.py
@@ -4,12 +4,12 @@ from general.models.item import Item
 
 class DocumentoDetalle(models.Model):    
     cantidad = models.FloatField(default=0)
-    precio = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    subtotal = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    descuento = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    total_bruto = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    porcentaje_descuento = models.DecimalField(max_digits=3, decimal_places=2, default=0)
+    precio = models.DecimalField(max_digits=12, decimal_places=6, default=0)
+    subtotal = models.DecimalField(max_digits=12, decimal_places=6, default=0)
+    descuento = models.DecimalField(max_digits=12, decimal_places=6, default=0)
+    total_bruto = models.DecimalField(max_digits=12, decimal_places=6, default=0)
+    total = models.DecimalField(max_digits=12, decimal_places=6, default=0)
+    porcentaje_descuento = models.DecimalField(max_digits=3, decimal_places=3, default=0)
     documento = models.ForeignKey(Documento, on_delete=models.CASCADE, related_name='detalles')
     documento_afectado = models.ForeignKey(Documento, on_delete=models.CASCADE, related_name='detalles_afectado', null=True)
     item = models.ForeignKey(Item, null=True, on_delete=models.CASCADE)
