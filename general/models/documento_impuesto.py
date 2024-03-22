0e273f71071d673e640d7b979d10410be0759e13
MarioA <mestradaz3@gmail.com>
2024-03-22 07:42:19 -0500

    Varios

--- general/models/documento_impuesto.py
+++ general/models/documento_impuesto.py
@@ -3,9 +3,9 @@ from general.models.documento_detalle import DocumentoDetalle
 from general.models.impuesto import Impuesto
 
 class DocumentoImpuesto(models.Model):   
-    base = models.DecimalField(max_digits=10, decimal_places=2, default=0)     
-    porcentaje = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    base = models.DecimalField(max_digits=12, decimal_places=6, default=0)     
+    porcentaje = models.DecimalField(max_digits=12, decimal_places=6, default=0)
+    total = models.DecimalField(max_digits=12, decimal_places=6, default=0)
     documento_detalle = models.ForeignKey(DocumentoDetalle, on_delete=models.CASCADE)
     impuesto = models.ForeignKey(Impuesto, on_delete=models.CASCADE)
 
