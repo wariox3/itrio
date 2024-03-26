4309847074891a909074f140a8653b3844725f0a
sebas957 <baxtian957@hotmail.com>
2024-03-26 10:17:31 -0500

    ajustes wolframio

--- utilidades/wolframio.py
+++ utilidades/wolframio.py
@@ -6,7 +6,7 @@ import json
 
 
 def consumirPost(data, url):
-    url = "http://localhost/wolframio/public/index.php" + url
+    url = "http://159.203.37.24/wolframio/public/index.php" + url
 
     json_data = json.dumps(data)
 
