9e7c38feabcdcccf5a0250818a660f20012fe85d
sebas957 <baxtian957@hotmail.com>
2024-04-03 10:04:49 -0500

    se elimina campo ambiente

--- general/models/resolucion.py
+++ general/models/resolucion.py
@@ -4,7 +4,6 @@ class Resolucion(models.Model):
     prefijo = models.CharField(max_length=50, null=True)
     numero = models.CharField(max_length=50, null=True)
     clave_tecnica = models.CharField(max_length=500, null=True)
-    ambiente = models.CharField(max_length=10, null=True)
     set_prueba = models.CharField(max_length=500, null=True)
     consecutivo_desde = models.IntegerField()
     consecutivo_hasta = models.IntegerField()
