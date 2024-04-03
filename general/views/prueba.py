b70fdd0a9d28dc5425e84f4323f01e266e3f25b9
MarioA <maestradaz3@gmail.com>
2024-04-02 20:16:12 -0500

    Varios

--- general/views/prueba.py
+++ general/views/prueba.py
@@ -15,15 +15,6 @@ def enviar_coreo(request):
 
 class PruebaView(APIView):
     def get(self, request):
-        for i in range(1, 15550):
-            item = Item(
-                codigo=i,
-                referencia=f"REF {i}",
-                nombre=f"Producto {i}",
-                costo=random.randrange(100000, 500000),
-                precio=random.randrange(500000, 1000000)
-                )
-            item.save()
         prueba = config('KEY_SENDGRID')
         return Response("Hola mundo" + prueba)
 
