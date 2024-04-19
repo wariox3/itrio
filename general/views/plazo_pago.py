11789fca17f77c21c408f35a9b2888f657cd89c8
sebas957 <baxtian957@hotmail.com>
2024-04-19 15:11:43 -0500

    ajustes plazo_pago

--- general/views/plazo_pago.py
+++ general/views/plazo_pago.py
@@ -8,22 +8,3 @@ class PlazoPagoViewSet(viewsets.ModelViewSet):
     queryset = PlazoPago.objects.all()
     serializer_class = PlazoPagoSerializador    
     permission_classes = [permissions.IsAuthenticated]        
-
-'''@action(detail=False, methods=["post"], url_path=r'lista',)
-def lista(self, request):
-    raw = request.data
-    desplazar = raw.get('desplazar', 0)
-    limite = raw.get('limite', 50)    
-    cantidadLimite = raw.get('cantidad_limite', 5000)    
-    filtros = raw.get('filtros')
-    ordenamientos = raw.get('ordenamientos')
-    plazoPagos = PlazoPago.objects.all()
-    if filtros:
-        for filtro in filtros:
-            plazoPagos = plazoPagos.filter(**{filtro['propiedad']: filtro['valor1']})
-    if ordenamientos:
-        plazoPagos = plazoPagos.order_by(*ordenamientos)              
-    plazoPagos = plazoPagos[desplazar:limite+desplazar]
-    plazoCantidad = PlazoPago.objects.all()[:cantidadLimite].count()
-    ItemSerializador = ItemSerializador(plazoPagos, many=True)
-    return Response({"registros": ItemSerializador.data, "cantidad_registros": plazoCantidad}, status=status.HTTP_200_OK)'''
