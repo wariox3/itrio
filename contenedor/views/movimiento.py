d05c8c725bf5b3f051acc20aaab45bebc4bbd4ae
MarioA <maestradaz3@gmail.com>
2024-06-13 09:57:33 -0500

    Varios

--- contenedor/views/movimiento.py
+++ contenedor/views/movimiento.py
@@ -3,6 +3,10 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from contenedor.models import ContenedorMovimiento
 from contenedor.serializers.movimiento import ContenedorMovimientoSerializador
+from decouple import config
+from datetime import timedelta
+from django.utils import timezone
+import hashlib
 
 class MovimientoViewSet(viewsets.ModelViewSet):
     queryset = ContenedorMovimiento.objects.all()
@@ -30,3 +34,21 @@ class MovimientoViewSet(viewsets.ModelViewSet):
             return Response({'movimientos':movimientosSerializador.data}, status=status.HTTP_200_OK)
         else:
             return Response({'Mensaje': 'Faltan parametros', 'codigo':1}, status=status.HTTP_400_BAD_REQUEST)            
+        
+    @action(detail=False, methods=["post"], permission_classes=[permissions.AllowAny], url_path=r'generar-integridad',)
+    def consulta_usuario(self, request):
+        raw = request.data
+        referencia = raw.get('referencia')
+        monto = raw.get('monto')
+        secreto_integridad = config('WOMPI_SECRETO_INTEGRIDAD')
+        if referencia and monto:                                
+            now = timezone.localtime(timezone.now())
+            now = now + timedelta(minutes=10)
+            fecha_vencimiento = now.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
+            cadena = referencia+monto+"COP"+str(fecha_vencimiento)+secreto_integridad
+            cadena_bytes = cadena.encode('utf-8')
+            hash_obj = hashlib.sha256(cadena_bytes)
+            hash_str = hash_obj.hexdigest()
+            return Response({'cadena_cruda':cadena, 'hash':hash_str}, status=status.HTTP_200_OK)
+        else:
+            return Response({'Mensaje': 'Faltan parametros', 'codigo':1}, status=status.HTTP_400_BAD_REQUEST)
