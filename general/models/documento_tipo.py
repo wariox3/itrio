a3c767c323afdc7055ed984e2e5592d12137d1c9
MarioA <mestradaz3@gmail.com>
2024-06-26 08:37:20 -0500

    varos

--- general/models/documento_tipo.py
+++ general/models/documento_tipo.py
@@ -6,6 +6,8 @@ from contabilidad.models.cuenta import Cuenta
 class DocumentoTipo(models.Model):    
     nombre = models.CharField(max_length=100)
     consecutivo = models.IntegerField(default=1)
+    venta = models.BooleanField(default = False)
+    compra = models.BooleanField(default = False)
     documento_clase = models.ForeignKey(DocumentoClase, null=True, on_delete=models.PROTECT, related_name='documentos_tipos_documento_clase')
     resolucion = models.ForeignKey(Resolucion, null=True, on_delete=models.PROTECT, related_name='documentos_tipos_resolucion')
     cuenta_cobrar = models.ForeignKey(Cuenta, null=True, on_delete=models.PROTECT, related_name='documentos_tipos_cuenta_cobrar')
