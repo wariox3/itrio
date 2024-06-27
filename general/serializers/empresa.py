60688a6a477d1e5c95da90a4f2628f3d0788a164
MarioA <mestradaz3@gmail.com>
2024-06-27 12:23:21 -0500

    Varios

--- general/serializers/empresa.py
+++ general/serializers/empresa.py
@@ -32,12 +32,23 @@ class EmpresaSerializador(serializers.HyperlinkedModelSerializer):
                 nombre_estado = instance.ciudad.estado.nombre
 
         nombre_completo = f"{nombre_ciudad} - {nombre_estado}"
+        tipo_persona_nombre = ""
+        if instance.tipo_persona:
+            tipo_persona_nombre = instance.tipo_persona.nombre
+        identificacion_nombre = ""
+        if instance.identificacion:
+            identificacion_nombre = instance.identificacion.nombre
+        regimen_nombre = ""
+        if instance.regimen:
+            regimen_nombre = instance.regimen.nombre
+
         region = config('DO_REGION')
         bucket = config('DO_BUCKET')
         return {
             'id': instance.id,            
             'numero_identificacion': instance.numero_identificacion,
             'identificacion_id': instance.identificacion_id,
+            'identificacion_nombre': identificacion_nombre,
             'ciudad_id': instance.ciudad_id,
             'ciudad_nombre': nombre_completo,
             'digito_verificacion': instance.digito_verificacion,
@@ -45,8 +56,10 @@ class EmpresaSerializador(serializers.HyperlinkedModelSerializer):
             'direccion': instance.direccion,
             'telefono': instance.telefono,
             'correo': instance.correo,
-            'regimen': instance.regimen_id,
-            'tipo_persona': instance.tipo_persona_id,
+            'regimen_id': instance.regimen_id,
+            'regimen_nombre': regimen_nombre,
+            'tipo_persona_id': instance.tipo_persona_id,
+            'tipo_persona_nombre': tipo_persona_nombre,
             'rededoc_id' : instance.rededoc_id,
             'imagen': f"https://{bucket}.{region}.digitaloceanspaces.com/{instance.imagen}"
         }   
