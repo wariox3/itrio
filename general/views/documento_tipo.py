880429ae6cee875edc0ecbfeeafbbd10fc254f1b
MarioA <mestradaz3@gmail.com>
2024-06-26 20:25:45 -0500

    Varios

--- general/views/documento_tipo.py
+++ general/views/documento_tipo.py
@@ -4,7 +4,7 @@ from general.models.documento_tipo import DocumentoTipo
 from general.serializers.documento_tipo import DocumentoTipoSerializador
 
 class DocumentoTipoViewSet(viewsets.ModelViewSet):
-    queryset = DocumentoTipo.objects.all()
+    queryset = DocumentoTipo.objects.all().order_by('id')
     serializer_class = DocumentoTipoSerializador
     permission_classes = [permissions.IsAuthenticated]
 
