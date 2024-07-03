0094026ddaa6b99292429952c3583cc26b4042ab
MarioA <mestradaz3@gmail.com>
2024-07-03 18:35:43 -0500

    Varios

--- general/views/prueba.py
+++ general/views/prueba.py
@@ -2,7 +2,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from general.models.item import Item
-from utilidades.correo import Correo
 from decouple import config 
 import random
 
