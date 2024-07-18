556406a1aac1fa684fef49ac1b2bce89f81b9160
MarioA <mestradaz3@gmail.com>
2024-07-18 15:09:39 -0500

    Varios

--- ruteo/serializers/rut_vehiculo.py
+++ ruteo/serializers/rut_vehiculo.py
@@ -5,12 +5,14 @@ from ruteo.models.rut_vehiculo import RutVehiculo
 class RutVehiculoSerializador(serializers.HyperlinkedModelSerializer):    
     class Meta:
         model = RutVehiculo
-        fields = ['id', 'placa', 'capacidad']
+        fields = ['id', 'placa', 'capacidad', 'estado_activo', 'estado_asignado']
 
     def to_representation(self, instance):        
         return {
             'id': instance.id, 
             'placa': instance.placa,
-            'capacidad': instance.capacidad
+            'capacidad': instance.capacidad,
+            'estado_activo': instance.estado_activo,
+            'estado_asignado': instance.estado_asignado
         }
     
