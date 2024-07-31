f3773d2491dba1b10783b31ed545eef0953b829a
MarioA <mestradaz3@gmail.com>
2024-07-31 09:40:43 -0500

    Varios

--- humano/serializers/hum_contrato.py
+++ humano/serializers/hum_contrato.py
@@ -12,4 +12,22 @@ class HumContratoSerializador(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = HumContrato
         fields = ['id', 'fecha_desde', 'fecha_hasta', 'estado_terminado', 'contrato_tipo', 'grupo', 'contacto']
+
+
+class HumContratoListaAutocompletarSerializador(serializers.HyperlinkedModelSerializer):
+    
+    class Meta:
+        model = HumContrato
+
+    def to_representation(self, instance):
+        contacto_numero_identificacion = ''
+        contacto_nombre_corto = ''
+        if instance.contacto:
+            contacto_numero_identificacion = instance.contacto.numero_identificacion
+            contacto_nombre_corto = instance.contacto.nombre_corto
+        return {
+            'contrato_id': instance.id,
+            'contrato_contacto_numero_identificacion': contacto_numero_identificacion,
+            'contrato_contacto_nombre_corto': contacto_nombre_corto
+        } 
         
