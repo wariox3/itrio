e62cc85ac334dace2024055a65b5589057d727ff
MarioA <mestradaz3@gmail.com>
2024-08-01 13:08:55 -0500

    Varios

--- humano/serializers/hum_adicional.py
+++ humano/serializers/hum_adicional.py
@@ -10,4 +10,27 @@ class HumAdicionalSerializador(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = HumAdicional
         fields = ['id', 'valor', 'horas', 'aplica_dia_laborado', 'detalle', 'concepto', 'contrato']
-        
+
+    def to_representation(self, instance):      
+        concepto_nombre = ''
+        if instance.concepto:
+            concepto_nombre = instance.concepto.nombre
+        contrato_contacto_numero_identificacion = ''
+        contrato_contacto_nombre_corto = ''
+        if instance.contrato:
+            if instance.contrato.contacto:
+                contrato_contacto_numero_identificacion = instance.contrato.contacto.numero_identificacion
+                contrato_contacto_nombre_corto = instance.contrato.contacto.nombre_corto
+        return {
+            'id': instance.id,
+            'valor': instance.valor,
+            'horas': instance.horas,
+            'aplica_dia_laborado': instance.aplica_dia_laborado,            
+            'detalle': instance.detalle,
+            'concepto_id': instance.concepto_id,
+            'concepto_nombre': concepto_nombre,
+            'contrato_id': instance.contrato_id,
+            'contrato_contacto_id': instance.contrato.contacto_id,
+            'contrato_contacto_numero_identificacion': contrato_contacto_numero_identificacion,
+            'contrato_contacto_nombre_corto': contrato_contacto_nombre_corto
+        }         
