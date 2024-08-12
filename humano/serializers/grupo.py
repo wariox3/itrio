51127350b06deb137af453d53fa01ad73e4a0d2a
MarioA <maestradaz3@gmail.com>
2024-08-12 17:05:51 -0500

    Varios

--- humano/serializers/grupo.py
+++ humano/serializers/grupo.py
@@ -17,3 +17,14 @@ class HumGrupoListaAutocompletarSerializador(serializers.HyperlinkedModelSeriali
             'grupo_id': instance.id,
             'grupo_nombre': instance.nombre,
         }         
+    
+class HumGrupoListaBuscarSerializador(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = HumGrupo
+        fields = ['id', 'nombre']
+
+    def to_representation(self, instance):
+        return {
+            'id': instance.id,
+            'nombre': instance.nombre
+        }     
