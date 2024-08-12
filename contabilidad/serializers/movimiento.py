09a05fb31a0c0dc8396491eb65bbc522895cbe2c
MarioA <maestradaz3@gmail.com>
2024-08-12 16:46:28 -0500

    Varios

--- contabilidad/serializers/movimiento.py
+++ contabilidad/serializers/movimiento.py
@@ -18,6 +18,24 @@ class ConMovimientoSerializador(serializers.HyperlinkedModelSerializer):
         fields = ['id', 'numero', 'fecha', 'debito', 'credito', 'base', 'naturaleza', 'cuenta', 'comprobante', 'contacto', 'documento', 'periodo']
 
     def to_representation(self, instance):
+        cuenta_codigo = ''
+        cuenta_nombre = ''
+        if instance.cuenta:
+            cuenta_codigo = instance.cuenta.codigo
+            cuenta_nombre = instance.cuenta.nombre
+        comprobante_codigo = ''
+        comprobante_nombre = ''
+        if instance.comprobante:
+            comprobante_codigo = instance.comprobante.codigo
+            comprobante_nombre = instance.comprobante.nombre
+        grupo_codigo = ''
+        grupo_nombre = ''
+        if instance.grupo:
+            grupo_codigo = instance.grupo.codigo
+            grupo_nombre = instance.grupo.nombre
+        contacto_nombre_corto = ''
+        if instance.contacto:
+            contacto_nombre_corto = instance.contacto.nombre_corto
         return {
             'id': instance.id,
             'numero': instance.numero,
@@ -27,8 +45,16 @@ class ConMovimientoSerializador(serializers.HyperlinkedModelSerializer):
             'base': instance.base ,
             'naturaleza': instance.naturaleza ,
             'cuenta_id': instance.cuenta_id,
+            'cuenta_codigo': cuenta_codigo,
+            'cuenta_nombre': cuenta_nombre,
             'comprobante_id': instance.comprobante_id,
+            'comprobante_codigo': comprobante_codigo,
+            'comprobante_nombre': comprobante_nombre,
+            'grupo_id': instance.grupo_id,
+            'grupo_codigo': grupo_codigo,
+            'grupo_nombre': grupo_nombre,
             'contacto_id': instance.contacto_id,
+            'contacto_nombre_corto': contacto_nombre_corto,
             'documento_id': instance.documento_id,
             'periodo_id': instance.periodo_id
         }         
