dd49aa66283a3af964bcc890d38a22c196473a9c
MarioA <mestradaz3@gmail.com>
2024-08-16 16:07:34 -0500

    Varios

--- general/views/configuracion.py
+++ general/views/configuracion.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from general.models.configuracion import GenConfiguracion
 from general.serializers.configuracion import GenConfiguracionSerializador, GenConfiguracionActualizarSerializador
 
@@ -18,3 +19,13 @@ class ConfiguracionViewSet(viewsets.ModelViewSet):
             configuracionSerializador.save()
             return Response({'actualizacion': True, 'configuracion': configuracionSerializador.data}, status=status.HTTP_201_CREATED)            
         return Response({'mensaje':'Errores en la actualizacion', 'codigo':23, 'validaciones': configuracionSerializador.errors}, status=status.HTTP_400_BAD_REQUEST)
+    
+    @action(detail=False, methods=["post"], url_path=r'consulta',)
+    def cosulta(self, request):
+        raw = request.data
+        campos = raw.get('campos')
+        if campos:
+            configuracion = GenConfiguracion.objects.filter(pk=1).values(*campos)
+            return Response({'configuracion': list(configuracion)}, status=status.HTTP_200_OK)   
+        else:
+            return Response({'mensaje':'Faltan parametros', 'codigo':1}, status=status.HTTP_400_BAD_REQUEST)
