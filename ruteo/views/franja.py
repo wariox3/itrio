cefdffda3c7f21ede62f4db308dd8afe8bab1421
MarioA <mestradaz3@gmail.com>
2024-08-21 18:39:49 -0500

    Varios

--- ruteo/views/franja.py
+++ ruteo/views/franja.py
@@ -10,8 +10,32 @@ from datetime import datetime
 from decouple import config
 import json
 from utilidades.zinc import Zinc
+from shapely.geometry import Point, Polygon
+
+def is_point_in_polygon(polygon_coords, point_coords):
+    polygon = Polygon(polygon_coords)
+    point = Point(point_coords)
+    return polygon.contains(point)
 
 class RutFranjaViewSet(viewsets.ModelViewSet):
     queryset = RutFranja.objects.all()
     serializer_class = RutFranjaSerializador
     permission_classes = [permissions.IsAuthenticated]
+
+    @action(detail=False, methods=["post"], url_path=r'ubicar',)
+    def ubicar(self, request):             
+        raw = request.data
+        latitud = raw.get('latitud')
+        longitud = raw.get('longitud')           
+
+        if latitud and longitud:
+            franja = RutFranja.objects.get(pk=1)
+            coordenadas = [(coord['longitud'], coord['latitud']) for coord in franja.coordenadas]
+            poligono = Polygon(coordenadas)                
+            punto = Point(latitud,longitud)            
+            if poligono.contains(punto):
+                return Response({'franja': 'SI'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'franja': 'NO'}, status=status.HTTP_200_OK)                                
+        else:
+            return Response({'mensaje':'Faltan parametros', 'codigo':1}, status=status.HTTP_400_BAD_REQUEST)
