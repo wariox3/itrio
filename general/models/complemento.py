30441d0feb84608f1f5fe1999a42bd6aa211541a
MarioA <mestradaz3@gmail.com>
2024-08-26 12:39:04 -0500

    Varios

--- general/models/complemento.py
+++ general/models/complemento.py
@@ -4,6 +4,7 @@ class GenComplemento(models.Model):
     id = models.BigIntegerField(primary_key=True)
     nombre = models.CharField(max_length=50) 
     instalado = models.BooleanField(default = False)
+    estructura_json = models.JSONField(null=True)
     datos_json = models.JSONField(null=True)
     
     class Meta:
--- general/serializers/complemento.py
+++ general/serializers/complemento.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class GenComplementoSerializador(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = GenComplemento
-        fields = ['id', 'nombre', 'instalado', 'datos_json']      
+        fields = ['id', 'nombre', 'instalado', 'estructura_json', 'datos_json']      
