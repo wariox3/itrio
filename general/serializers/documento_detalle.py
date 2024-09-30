f0ecc6a3e7b1485dcd4d0a3c976c6b743e0f1a90
MarioA <mestradaz3@gmail.com>
2024-09-30 11:52:06 -0500

    Varios

--- general/serializers/documento_detalle.py
+++ general/serializers/documento_detalle.py
@@ -19,7 +19,7 @@ class GenDocumentoDetalleSerializador(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = GenDocumentoDetalle
         fields = ['documento', 'documento_afectado', 'item', 'cuenta', 'contacto', 'cantidad', 'precio', 'pago', 'porcentaje_descuento', 
-                  'porcentaje', 'descuento', 'subtotal', 'total_bruto', 'total', 'base_impuesto', 'hora', 'naturaleza', 'impuesto', 
+                  'porcentaje', 'descuento', 'subtotal', 'total_bruto', 'total', 'base_impuesto', 'hora', 'naturaleza', 'impuesto', 'impuesto_operado', 
                   'detalle', 'numero', 'concepto', 'credito', 'base_cotizacion', 'base_prestacion', 'operacion', 'pago_operado', 
                   'devengado', 'deduccion', 'dias']
 
@@ -64,6 +64,7 @@ class GenDocumentoDetalleSerializador(serializers.HyperlinkedModelSerializer):
             'total_bruto' : instance.total_bruto,
             'base_impuesto' : instance.base_impuesto,
             'impuesto' : instance.impuesto,
+            'impuesto_operado' : instance.impuesto_operado,
             'total' : instance.total,
             'hora' : instance.hora,
             'dias' : instance.dias,
