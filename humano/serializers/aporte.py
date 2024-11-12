7687f66642ad3a30f5e532377357e8cc9dcaec81
MarioA <mestradaz3@gmail.com>
2024-11-12 09:06:02 -0500

    Varios

--- humano/serializers/aporte.py
+++ humano/serializers/aporte.py
@@ -9,4 +9,25 @@ class HumAporteSerializador(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = HumAporte
-        fields = ['id', 'fecha_desde', 'fecha_hasta', 'fecha_hasta_periodo', 'anio', 'mes', 'anio_salud', 'mes_salud', 'sucursal']        
+        fields = ['id', 'fecha_desde', 'fecha_hasta', 'fecha_hasta_periodo', 'anio', 'mes', 'anio_salud', 'mes_salud', 'sucursal',
+                  'estado_generado', 'estado_aprobado']      
+        
+    def to_representation(self, instance):      
+        sucursal_nombre = ''
+        if instance.sucursal:
+            sucursal_nombre = instance.sucursal.nombre    
+        return {
+            'id': instance.id,
+            'fecha_desde': instance.fecha_desde,
+            'fecha_hasta': instance.fecha_hasta,
+            'fecha_hasta_periodo': instance.fecha_hasta_periodo,
+            'anio': instance.anio,
+            'mes': instance.mes,
+            'anio_salud': instance.anio_salud,
+            'mes_salud': instance.mes_salud,
+            'estado_generado': instance.estado_generado,
+            'estado_aprobado': instance.estado_aprobado,
+            'sucursal_id': instance.sucursal_id,
+            'sucursal_nombre': sucursal_nombre            
+
+        }          
