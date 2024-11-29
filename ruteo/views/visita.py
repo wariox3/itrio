5d546e3815deb7dacec2fc76717d6f7e193872df
MarioA <mestradaz3@gmail.com>
2024-11-29 15:20:01 -0500

    Varios

--- ruteo/views/visita.py
+++ ruteo/views/visita.py
@@ -353,8 +353,16 @@ class RutVisitaViewSet(viewsets.ModelViewSet):
             return Response({'mensaje':'Faltan parametros', 'codigo':1}, status=status.HTTP_400_BAD_REQUEST)
 
     @action(detail=False, methods=["post"], url_path=r'ordenar',)
-    def ordenar(self, request):        
-        visitas = RutVisita.objects.filter(estado_despacho=False, estado_decodificado=True).values('id', 'latitud', 'longitud')
+    def ordenar(self, request):      
+        raw = request.data
+        filtros = raw.get('filtros')
+        visitas = RutVisita.objects.filter(estado_decodificado=True)
+        if filtros:
+            for filtro in filtros:
+                visitas = visitas.filter(**{filtro['propiedad']: filtro['valor1']})
+                    
+        visitas = visitas.values('id', 'latitud', 'longitud')  
+        #visitas = RutVisita.objects.filter(estado_despacho=False, estado_decodificado=True).values('id', 'latitud', 'longitud')
         if visitas.exists():
             cantidad_visitas = len(visitas)   
             punto_inicial = (6.200479, -75.586350)     
@@ -402,54 +410,7 @@ class RutVisitaViewSet(viewsets.ModelViewSet):
 
             return Response({'mensaje':'visitas ordenadas', 'orden': orden}, status=status.HTTP_200_OK)
         else:
-            return Response({'mensaje': 'No hay visitas pendientes por ordenar'}, status=status.HTTP_200_OK) 
-
-    @action(detail=False, methods=["post"], url_path=r'ordenar2',)
-    def ordenar2(self, request):        
-        visitas = RutVisita.objects.filter(estado_despacho=False, estado_decodificado=True).values('id', 'latitud', 'longitud')
-        if visitas.exists():
-            num_locations = len(visitas)
-
-            distance_matrix = [[
-                calcular_distancia(loc1['latitud'], loc1['longitud'], loc2['latitud'], loc2['longitud'])
-                for loc2 in visitas
-            ] for loc1 in visitas]
-
-            # Configurar el problema de enrutamiento
-            manager = pywrapcp.RoutingIndexManager(num_locations, 1, 0)
-            routing = pywrapcp.RoutingModel(manager)
-
-            # Función de costo
-            def distance_callback(from_index, to_index):
-                from_node = manager.IndexToNode(from_index)
-                to_node = manager.IndexToNode(to_index)
-                return distance_matrix[from_node][to_node]
-
-            transit_callback_index = routing.RegisterTransitCallback(distance_callback)
-            routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
-
-            # Resolver el problema
-            search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-            search_parameters.first_solution_strategy = (
-                routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
-            solution = routing.SolveWithParameters(search_parameters)
-
-            if solution:
-                # Extraer la ruta optimizada y asignar el orden
-                route = []
-                index = routing.Start(0)
-                while not routing.IsEnd(index):
-                    route.append(manager.IndexToNode(index))
-                    index = solution.Value(routing.NextVar(index))
-
-                # Actualizar el orden en la base de datos
-                for order, visit_index in enumerate(route):
-                    visita_id = visitas[visit_index]['id']
-                    RutVisita.objects.filter(id=visita_id).update(orden=order + 1)
-
-            return Response({'mensaje':'visitas ordenadas'}, status=status.HTTP_200_OK)
-        else:
-            return Response({'mensaje': 'No hay visitas pendientes por ordenar'}, status=status.HTTP_200_OK) 
+            return Response({'mensaje': 'No hay visitas pendientes por ordenar'}, status=status.HTTP_200_OK)  
 
     @action(detail=False, methods=["post"], url_path=r'ordenar-google',)
     def ordenar_google(self, request):        
@@ -498,9 +459,15 @@ class RutVisitaViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=["post"], url_path=r'rutear',)
     def rutear(self, request):
+        raw = request.data
+        filtros = raw.get('filtros')
         flota = RutFlota.objects.all()        
         if flota.exists():
-            visitas = RutVisita.objects.filter(estado_despacho = False).order_by('orden')                            
+            visitas = RutVisita.objects.all()
+            if filtros:
+                for filtro in filtros:
+                    visitas = visitas.filter(**{filtro['propiedad']: filtro['valor1']})
+            visitas = visitas.order_by('orden')                                        
             cantidad_vehiculos = len(flota)
             vehiculo_indice = 0
             vehiculo = flota[vehiculo_indice].vehiculo            
@@ -583,7 +550,13 @@ class RutVisitaViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=["post"], url_path=r'resumen',)
     def resumen(self, request):
-        visitas = RutVisita.objects.filter(estado_despacho = False).aggregate(
+        raw = request.data
+        filtros = raw.get('filtros')
+        visitas = RutVisita.objects.all()
+        if filtros:
+            for filtro in filtros:
+                visitas = visitas.filter(**{filtro['propiedad']: filtro['valor1']})
+        visitas = visitas.aggregate(
             cantidad=Count('id'), 
             peso=Coalesce(Sum('peso'), 0.0)
             )     
