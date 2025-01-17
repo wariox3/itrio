d556ac71289708a6a22a864f38d3c73903afa4b9
sebas957 <baxtian957@hotmail.com>
2025-01-17 14:28:30 -0500

    se ajusta tamaño de los excel

--- utilidades/excel.py
+++ utilidades/excel.py
@@ -13,26 +13,30 @@ class WorkbookEstilos(Workbook):
     def aplicar_estilos(self):
         for sheet in self.wb.sheetnames:
             ws = self.wb[sheet]
+            
+            # Aplicar estilos por celda
             for row in ws.iter_rows():
                 for cell in row:
                     cell.font = self.font
-                    #cell.alignment = self.alignment
-                    if cell.row == 1:  # Si es la primera fila
+                    if cell.row == 1:  # Si es la primera fila (encabezado)
                         cell.font = self.bold_font
                         cell.fill = self.fill
                     if isinstance(cell.value, (int, float)):
                         cell.alignment = Alignment(horizontal='right')                         
                     if isinstance(cell.value, bool):
-                        cell.value = "SI" if cell.value else "NO"                                               
+                        cell.value = "SI" if cell.value else "NO"
 
-        for column in ws.columns:
-            max_length = 0
-            column_letter = get_column_letter(column[0].column)
-            for cell in column:
-                try:
-                    if len(str(cell.value)) > max_length:
-                        max_length = len(cell.value)
-                except:
-                    pass
-            adjusted_width = (max_length + 2) * 1.2
-            ws.column_dimensions[column_letter].width = adjusted_width                      
+            # Ajustar el ancho de las columnas según el contenido más amplio
+            for column in ws.columns:
+                max_length = 0
+                column_letter = get_column_letter(column[0].column)
+                for cell in column:
+                    try:
+                        if cell.value is not None:  # Asegurarse de que no sea None
+                            max_length = max(max_length, len(str(cell.value)))
+                    except Exception as e:
+                        print(f"Error al calcular el ancho para la celda {cell.coordinate}: {e}")
+                
+                # Ajustar el ancho de la columna con un margen adicional
+                adjusted_width = (max_length + 2)
+                ws.column_dimensions[column_letter].width = adjusted_width
