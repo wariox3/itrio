5e15d2dc93f1f6e62bf524a5767b722c8c5cd6d3
sebas957 <baxtian957@hotmail.com>
2025-02-08 10:22:10 -0500

    ajustes configuracion

--- general/serializers/configuracion.py
+++ general/serializers/configuracion.py
@@ -1,6 +1,7 @@
 from general.models.configuracion import GenConfiguracion
 from general.models.empresa import GenEmpresa
 from humano.models.entidad import HumEntidad
+from contabilidad.models.cuenta import ConCuenta
 
 from rest_framework import serializers
 from decouple import config
@@ -8,20 +9,24 @@ from decouple import config
 class GenConfiguracionSerializador(serializers.HyperlinkedModelSerializer):
     empresa = serializers.PrimaryKeyRelatedField(queryset=GenEmpresa.objects.all())
     hum_entidad_riesgo = serializers.PrimaryKeyRelatedField(queryset=HumEntidad.objects.all(), default=None, allow_null=True)
+    cuenta_cobrar = serializers.PrimaryKeyRelatedField(queryset=ConCuenta.objects.all(), default=None, allow_null=True)
+    cuenta_pagar = serializers.PrimaryKeyRelatedField(queryset=ConCuenta.objects.all(), default=None, allow_null=True)
     class Meta:
         model = GenConfiguracion
         fields = ['id', 'empresa', 'informacion_factura', 'informacion_factura_superior',
             'venta_asesor', 'venta_sede', 
             'gen_uvt',
-            'hum_factor', 'hum_salario_minimo', 'hum_auxilio_transporte', 'hum_entidad_riesgo'
+            'hum_factor', 'hum_salario_minimo', 'hum_auxilio_transporte', 'hum_entidad_riesgo', 'cuenta_pagar', 'cuenta_cobrar'
         ]      
         
 class GenConfiguracionActualizarSerializador(serializers.HyperlinkedModelSerializer):
     hum_entidad_riesgo = serializers.PrimaryKeyRelatedField(queryset=HumEntidad.objects.all(), default=None, allow_null=True)
+    cuenta_cobrar = serializers.PrimaryKeyRelatedField(queryset=ConCuenta.objects.all(), default=None, allow_null=True)
+    cuenta_pagar = serializers.PrimaryKeyRelatedField(queryset=ConCuenta.objects.all(), default=None, allow_null=True)
     class Meta:
         model = GenConfiguracion
         fields = ['informacion_factura', 'informacion_factura_superior', 'venta_asesor',
             'venta_sede', 
             'gen_uvt',
-            'hum_factor', 'hum_salario_minimo', 'hum_auxilio_transporte', 'hum_entidad_riesgo'
+            'hum_factor', 'hum_salario_minimo', 'hum_auxilio_transporte', 'hum_entidad_riesgo', 'cuenta_pagar', 'cuenta_cobrar'
         ]
