d94b86c6c205b3fb93afcc46cc5adaead4ce05d0
sebas957 <baxtian957@hotmail.com>
2025-02-10 08:14:10 -0500

    ajustes error de configuracion

--- general/serializers/configuracion.py
+++ general/serializers/configuracion.py
@@ -9,24 +9,21 @@ from decouple import config
 class GenConfiguracionSerializador(serializers.HyperlinkedModelSerializer):
     empresa = serializers.PrimaryKeyRelatedField(queryset=GenEmpresa.objects.all())
     hum_entidad_riesgo = serializers.PrimaryKeyRelatedField(queryset=HumEntidad.objects.all(), default=None, allow_null=True)
-    cuenta_cobrar = serializers.PrimaryKeyRelatedField(queryset=ConCuenta.objects.all(), default=None, allow_null=True)
-    cuenta_pagar = serializers.PrimaryKeyRelatedField(queryset=ConCuenta.objects.all(), default=None, allow_null=True)
     class Meta:
         model = GenConfiguracion
         fields = ['id', 'empresa', 'informacion_factura', 'informacion_factura_superior',
             'venta_asesor', 'venta_sede', 
             'gen_uvt',
-            'hum_factor', 'hum_salario_minimo', 'hum_auxilio_transporte', 'hum_entidad_riesgo', 'cuenta_pagar', 'cuenta_cobrar'
+            'hum_factor', 'hum_salario_minimo', 'hum_auxilio_transporte', 'hum_entidad_riesgo'
         ]      
         
 class GenConfiguracionActualizarSerializador(serializers.HyperlinkedModelSerializer):
     hum_entidad_riesgo = serializers.PrimaryKeyRelatedField(queryset=HumEntidad.objects.all(), default=None, allow_null=True)
-    cuenta_cobrar = serializers.PrimaryKeyRelatedField(queryset=ConCuenta.objects.all(), default=None, allow_null=True)
-    cuenta_pagar = serializers.PrimaryKeyRelatedField(queryset=ConCuenta.objects.all(), default=None, allow_null=True)
+
     class Meta:
         model = GenConfiguracion
         fields = ['informacion_factura', 'informacion_factura_superior', 'venta_asesor',
             'venta_sede', 
             'gen_uvt',
-            'hum_factor', 'hum_salario_minimo', 'hum_auxilio_transporte', 'hum_entidad_riesgo', 'cuenta_pagar', 'cuenta_cobrar'
+            'hum_factor', 'hum_salario_minimo', 'hum_auxilio_transporte', 'hum_entidad_riesgo',
         ]
