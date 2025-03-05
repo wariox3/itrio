64c7b95a8904ad6860169b9af2e5155031469f21
MarioA <maestradaz3@gmail.com>
2025-03-05 17:01:06 -0500

    feat: grupo en sede

--- general/models/sede.py
+++ general/models/sede.py
@@ -1,7 +1,9 @@
 from django.db import models
+from contabilidad.models.grupo import ConGrupo
 
 class GenSede(models.Model):        
     nombre = models.CharField(max_length=100)
+    grupo = models.ForeignKey(ConGrupo, null=True, on_delete=models.PROTECT, related_name='sedes_grupo_rel')
 
     class Meta:
         db_table = "gen_sede"
--- general/serializers/sede.py
+++ general/serializers/sede.py
@@ -1,18 +1,23 @@
 from general.models.sede import GenSede
 from rest_framework import serializers
+from contabilidad.models.grupo import ConGrupo
 
 class GenSedeSerializador(serializers.HyperlinkedModelSerializer):
+    grupo = serializers.PrimaryKeyRelatedField(queryset=ConGrupo.objects.all(), default=None, allow_null=True)
     class Meta:
         model = GenSede
-        fields = [
-            'id',
-            'nombre', 
-            ]  
+        fields = ['id', 'nombre', 'grupo']  
         
     def to_representation(self, instance):
+        grupo_nombre = ""
+        if instance.grupo:
+            grupo_nombre = instance.grupo.nombre
+
         return {
             'id': instance.id,
-            'nombre': instance.nombre    
+            'nombre': instance.nombre,
+            'grupo_id': instance.grupo_id,
+            'grupo_nombre': grupo_nombre    
         }     
 
 class GenSedeListaAutocompletarSerializador(serializers.HyperlinkedModelSerializer):
