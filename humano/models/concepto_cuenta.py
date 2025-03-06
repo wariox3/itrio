5c96b7a1ea23fac98e6221cfd7370ba8911a3a3b
MarioA <maestradaz3@gmail.com>
2025-03-06 14:26:08 -0500

    feat: adicionar campo

--- humano/models/concepto_cuenta.py
+++ humano/models/concepto_cuenta.py
@@ -1,9 +1,11 @@
 from django.db import models
 from humano.models.concepto import HumConcepto
+from humano.models.tipo_costo import HumTipoCosto
 from contabilidad.models.cuenta import ConCuenta
 
 class HumConceptoCuenta(models.Model):            
     concepto = models.ForeignKey(HumConcepto, on_delete=models.PROTECT, related_name='conceptos_cuentas_concepto_rel')
+    tipo_costo = models.ForeignKey(HumTipoCosto, on_delete=models.PROTECT, related_name='conceptos_cuentas_tipo_costo_rel')
     cuenta = models.ForeignKey(ConCuenta, on_delete=models.PROTECT, related_name='conceptos_cuentas_cuenta_rel')
 
     class Meta:
--- humano/serializers/concepto_cuenta.py
+++ humano/serializers/concepto_cuenta.py
@@ -1,20 +1,23 @@
 from rest_framework import serializers
 from humano.models.concepto_cuenta import HumConceptoCuenta
 from humano.models.concepto import HumConcepto
+from humano.models.tipo_costo import HumTipoCosto
 from contabilidad.models.cuenta import ConCuenta
 
 class HumConceptoCuentaSerializador(serializers.HyperlinkedModelSerializer):
     concepto = serializers.PrimaryKeyRelatedField(queryset=HumConcepto.objects.all())
+    tipo_costo = serializers.PrimaryKeyRelatedField(queryset=HumTipoCosto.objects.all())
     cuenta = serializers.PrimaryKeyRelatedField(queryset=ConCuenta.objects.all())
     
     class Meta:
         model = HumConceptoCuenta
-        fields = ['concepto', 'cuenta']
+        fields = ['concepto', 'tipo_costo', 'cuenta']
 
     def to_representation(self, instance):       
         return {
             'id': instance.id,         
             'concepto_id': instance.concepto_id,
+            'tipo_costo_id': instance.tipo_costo_id,
             'cuenta_id': instance.cuenta_id
         } 
        
