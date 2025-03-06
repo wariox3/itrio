92279e0666f2a3ff6a5110775838a80643b42f3a
MarioA <maestradaz3@gmail.com>
2025-03-06 08:30:17 -0500

    feat: varios

--- humano/models/pago_tipo.py
+++ humano/models/pago_tipo.py
@@ -1,10 +1,11 @@
 from django.db import models
+from contabilidad.models.cuenta import ConCuenta
 
 class HumPagoTipo(models.Model):  
     id = models.BigIntegerField(primary_key=True)      
     nombre = models.CharField(max_length=100)    
-    consecutivo = models.IntegerField(default=1)
     aplica_programacion = models.BooleanField(default = False)
+    cuenta = models.ForeignKey(ConCuenta, null=True, on_delete=models.PROTECT, related_name='pagos_tipos_cuenta_rel')
 
     class Meta:
         db_table = "hum_pago_tipo"
--- humano/serializers/pago_tipo.py
+++ humano/serializers/pago_tipo.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from humano.models.pago_tipo import HumPagoTipo
+from contabilidad.models.cuenta import ConCuenta
 
 class HumPagoTipoSerializador(serializers.HyperlinkedModelSerializer):
+    cuenta = serializers.PrimaryKeyRelatedField(queryset=ConCuenta.objects.all(), default=None, allow_null=True)
     
     class Meta:
         model = HumPagoTipo
-        fields = ['id', 'nombre']
+        fields = ['id', 'nombre', 'cuenta']
 
 class HumPagoTipoListaAutocompletarSerializador(serializers.HyperlinkedModelSerializer):
     
