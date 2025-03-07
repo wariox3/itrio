2bb3ddfa082110d959dc5f5dc5992d4d8e3bef50
sebas957 <baxtian957@hotmail.com>
2025-03-07 13:45:33 -0500

    ajustes configuracion

--- humano/serializers/configuracion_provision.py
+++ humano/serializers/configuracion_provision.py
@@ -10,7 +10,7 @@ class HumConfiguracionProvisionSerializador(serializers.HyperlinkedModelSerializ
 
     class Meta:
         model = HumConfiguracionProvision
-        fields = ['id', 'nombre']    
+        fields = ['id', 'nombre', 'cuenta_credito', 'cuenta_debito']    
 
     def to_representation(self, instance):       
         tipo_costo_nombre = ""
