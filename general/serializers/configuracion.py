3114b323a0428e2f88e570dafe9eb63fd73c3cba
MarioA <mestradaz3@gmail.com>
2025-03-17 13:53:33 -0500

    feat: actualizar parcial configuracion

--- general/serializers/configuracion.py
+++ general/serializers/configuracion.py
@@ -17,14 +17,3 @@ class GenConfiguracionSerializador(serializers.HyperlinkedModelSerializer):
             'hum_factor', 'hum_salario_minimo', 'hum_auxilio_transporte', 'hum_entidad_riesgo',
             'pos_documento_tipo'
         ]      
-        
-class GenConfiguracionActualizarSerializador(serializers.HyperlinkedModelSerializer):
-    hum_entidad_riesgo = serializers.PrimaryKeyRelatedField(queryset=HumEntidad.objects.all(), default=None, allow_null=True)
-    pos_documento_tipo = serializers.PrimaryKeyRelatedField(queryset=GenDocumentoTipo.objects.all(), default=None, allow_null=True)
-    class Meta:
-        model = GenConfiguracion
-        fields = ['informacion_factura', 'informacion_factura_superior',
-            'gen_uvt',
-            'hum_factor', 'hum_salario_minimo', 'hum_auxilio_transporte', 'hum_entidad_riesgo',
-            'pos_documento_tipo'
-        ]
--- general/views/configuracion.py
+++ general/views/configuracion.py
@@ -2,23 +2,13 @@ from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from general.models.configuracion import GenConfiguracion
-from general.serializers.configuracion import GenConfiguracionSerializador, GenConfiguracionActualizarSerializador
+from general.serializers.configuracion import GenConfiguracionSerializador
 
 class ConfiguracionViewSet(viewsets.ModelViewSet):
     queryset = GenConfiguracion.objects.all()
     serializer_class = GenConfiguracionSerializador    
     permission_classes = [permissions.IsAuthenticated]
 
-    def create(self, request):
-        pass
-        
-    def update(self, request, pk=None):
-        configuracion = self.get_object()
-        configuracionSerializador = GenConfiguracionActualizarSerializador(configuracion, data=request.data)
-        if configuracionSerializador.is_valid():
-            configuracionSerializador.save()
-            return Response({'actualizacion': True, 'configuracion': configuracionSerializador.data}, status=status.HTTP_201_CREATED)            
-        return Response({'mensaje':'Errores en la actualizacion', 'codigo':23, 'validaciones': configuracionSerializador.errors}, status=status.HTTP_400_BAD_REQUEST)
     
     @action(detail=False, methods=["post"], url_path=r'consulta',)
     def cosulta(self, request):
