33fdc07d8565475aa676a907096f83a8ea78ca19
MarioA <mestradaz3@gmail.com>
2025-03-19 10:04:20 -0500

    feat: adicionar modelo para manejar las imagenes

--- general/models/archivo.py
+++ general/models/archivo.py
@@ -2,12 +2,15 @@ from django.db import models
 from general.models.documento import GenDocumento
 
 class GenArchivo(models.Model):    
-    fecha = models.DateTimeField(auto_now_add=True)
+    archivo_tipo_id = models.IntegerField(default=1)
+    fecha = models.DateTimeField(auto_now_add=True)    
     nombre = models.CharField(max_length=500)    
     tipo = models.CharField(max_length=100)
     tamano = models.DecimalField(max_digits=20, decimal_places=6, default=0)
     almacenamiento_id = models.CharField(max_length=255)
     uuid = models.CharField(max_length=100, null=True)
+    codigo = models.IntegerField(null=True)
+    modelo = models.CharField(max_length=100, null=True)
     documento = models.ForeignKey(GenDocumento, null=True, on_delete=models.PROTECT, related_name='archivos_documento_rel')
 
     class Meta:
