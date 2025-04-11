cc5f5e5dad6f147ab9f675006bf11a5336a2774a
MarioA <maestradaz3@gmail.com>
2025-04-11 13:09:01 -0500

    feat: Modulos de los planes

--- contenedor/models.py
+++ contenedor/models.py
@@ -71,7 +71,14 @@ class CtnPlan(models.Model):
     precio_usuario_adicional = models.DecimalField(max_digits=16, decimal_places=2, default=0)
     limite_electronicos = models.DecimalField(max_digits=16, decimal_places=2, default=0)
     plan_tipo_id = models.CharField(max_length=1, null=True) 
-    orden = models.IntegerField(default=0)
+    orden = models.IntegerField(default=0)        
+    compra = models.BooleanField(default=False)
+    tesoreria = models.BooleanField(default=False)
+    venta = models.BooleanField(default=False)
+    cartera = models.BooleanField(default=False)
+    inventario = models.BooleanField(default=False)
+    humano = models.BooleanField(default=False)
+    contabilidad = models.BooleanField(default=False)
 
     class Meta:
         db_table = "cnt_plan"
