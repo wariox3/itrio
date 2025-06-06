b8f436e468dffb4f13ad29fbaf51d4f4a06b0c80
MarioA <maestradaz3@gmail.com>
2025-06-06 11:45:50 -0500

    Feat: nuevo filtro

--- general/filters/documento.py
+++ general/filters/documento.py
@@ -1,7 +1,10 @@
 import django_filters
 from general.models.documento import GenDocumento
+from django_filters import NumberFilter
 
 class DocumentoFilter(django_filters.FilterSet):
+    id__gte = NumberFilter(field_name='id', lookup_expr='gte')
+    id__lte = NumberFilter(field_name='id', lookup_expr='lte')
     contacto_numero_identificacion = django_filters.CharFilter(field_name='contacto__numero_identificacion', lookup_expr='icontains')
     class Meta:
         model = GenDocumento        
