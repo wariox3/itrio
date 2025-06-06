e51aba236110245c85905e348b7506257a65f531
MarioA <maestradaz3@gmail.com>
2025-06-06 12:11:54 -0500

    Feat: nuevo filtro

--- general/filters/documento.py
+++ general/filters/documento.py
@@ -3,16 +3,13 @@ from general.models.documento import GenDocumento
 from django_filters import NumberFilter
 
 class DocumentoFilter(django_filters.FilterSet):
-    id__gte = NumberFilter(field_name='id', lookup_expr='gte')
-    id__lte = NumberFilter(field_name='id', lookup_expr='lte')
     contacto_numero_identificacion = django_filters.CharFilter(field_name='contacto__numero_identificacion', lookup_expr='icontains')
     class Meta:
         model = GenDocumento        
-        fields = ['id',
-                  'documento_tipo_id',
-                  'numero', 
-                  'contacto_numero_identificacion',
-                  'estado_aprobado', 
-                  'estado_anulado', 
-                  'estado_electronico', 
-                  'estado_contabilizado']
+        fields = {'id': ['exact', 'lte'],
+                  'documento_tipo_id': ['exact'],
+                  'numero': ['exact'], 
+                  'estado_aprobado': ['exact'], 
+                  'estado_anulado': ['exact'], 
+                  'estado_electronico': ['exact'], 
+                  'estado_contabilizado': ['exact']}
