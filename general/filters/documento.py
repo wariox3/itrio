15190884bbabe7a95151904b48ca33db62519850
MarioA <maestradaz3@gmail.com>
2025-06-11 11:46:10 -0500

    feat: varios

--- general/filters/documento.py
+++ general/filters/documento.py
@@ -4,11 +4,13 @@ from django_filters import NumberFilter
 
 class DocumentoFilter(django_filters.FilterSet):
     contacto_numero_identificacion = django_filters.CharFilter(field_name='contacto__numero_identificacion', lookup_expr='icontains')
+    documento_tipo__cobrar = django_filters.BooleanFilter(field_name='documento_tipo__cobrar')
     class Meta:
         model = GenDocumento        
         fields = {'id': ['exact', 'lte'],
                   'documento_tipo_id': ['exact'],
                   'numero': ['exact'], 
+                  'pendiente':['gt'],
                   'estado_aprobado': ['exact'], 
                   'estado_anulado': ['exact'], 
                   'estado_electronico': ['exact'], 
