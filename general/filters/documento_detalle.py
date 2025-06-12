77c7d3c6fe4433c249acd86c41dff549b889ddc1
MarioA <maestradaz3@gmail.com>
2025-06-12 08:33:41 -0500

    correccion

--- general/filters/documento_detalle.py
+++ general/filters/documento_detalle.py
@@ -2,8 +2,9 @@ import django_filters
 from general.models.documento_detalle import GenDocumentoDetalle
 from django_filters import NumberFilter
 
-class DocumentoDetalleFilter(django_filters.FilterSet):    
+class DocumentoDetalleFilter(django_filters.FilterSet):     
     class Meta:
         model = GenDocumentoDetalle        
         fields = {'id': ['exact', 'lte'],
-                  'documento__estado_aprobado':['exact']}
+                  'documento__estado_aprobado':['exact'],
+                  'documento__documento_tipo__venta':['exact']}
