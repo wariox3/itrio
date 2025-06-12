19b59bbe026b78fac5fb3382da36f2a4c27eda65
sebas957 <baxtian957@hotmail.com>
2025-06-12 11:06:50 -0500

    filters para documento tipo

---
+++ general/filters/documento_tipo.py
@@ -0,0 +1,9 @@
+import django_filters
+from general.models.documento_tipo import GenDocumentoTipo
+from django_filters import NumberFilter
+
+class DocumentoTipoFilter(django_filters.FilterSet):
+    class Meta:
+        model = GenDocumentoTipo        
+        fields = {'venta': ['exact'], 
+                  'operacion': ['exact']}
