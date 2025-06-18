ff4d3753a49d6359285a28f51e59a00b1105f2da
sebas957 <baxtian957@hotmail.com>
2025-06-18 10:12:35 -0500

    ajustes get

--- humano/views/motivo_terminacion.py
+++ humano/views/motivo_terminacion.py
@@ -1,8 +1,30 @@
 from rest_framework import viewsets, permissions
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from humano.models.motivo_terminacion import HumMotivoTerminacion
 from humano.serializers.motivo_terminacion import HumMotivoTerminacionSerializador
+from humano.filters.motivo_terminacion import MotivoTerminacionFilter
 
 class HumMotivoTerminacionViewSet(viewsets.ModelViewSet):
     queryset = HumMotivoTerminacion.objects.all()
     serializer_class = HumMotivoTerminacionSerializador
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_class = MotivoTerminacionFilter 
+
+    def get_serializer_class(self):
+        serializador_parametro = self.request.query_params.get('serializador', None)
+        if not serializador_parametro or serializador_parametro not in self.serializadores:
+            return HumMotivoTerminacionSerializador
+        return self.serializadores[serializador_parametro]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        serializer_class = self.get_serializer_class()        
+        select_related = getattr(serializer_class.Meta, 'select_related_fields', [])
+        if select_related:
+            queryset = queryset.select_related(*select_related)        
+        campos = serializer_class.Meta.fields        
+        if campos and campos != '__all__':
+            queryset = queryset.only(*campos) 
+        return queryset 
