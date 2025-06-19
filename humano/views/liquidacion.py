031930245b3914bce90ffed6a72d3c85d2d7779b
MarioA <maestradaz3@gmail.com>
2025-06-19 10:44:06 -0500

    feat: desaprobar liquidacion

--- humano/views/liquidacion.py
+++ humano/views/liquidacion.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from humano.models.liquidacion import HumLiquidacion
+from general.models.documento import GenDocumento
 from humano.serializers.liquidacion import HumLiquidacionSerializador, HumLiquidacionListaSerializador
 from humano.filters.liquidacion import LiquidacionFilter
 
@@ -59,8 +60,7 @@ class HumLiquidacionViewSet(viewsets.ModelViewSet):
             else:
                 return Response({'mensaje':'Faltan parametros', 'codigo':1}, status=status.HTTP_400_BAD_REQUEST)
         except HumLiquidacion.DoesNotExist:
-            return Response({'mensaje':'La liquidación no existe', 'codigo':15}, status=status.HTTP_400_BAD_REQUEST)     
-        
+            return Response({'mensaje':'La liquidación no existe', 'codigo':15}, status=status.HTTP_400_BAD_REQUEST)             
 
     @action(detail=False, methods=["post"], url_path=r'desgenerar',)
     def desgenerar(self, request):
@@ -81,7 +81,6 @@ class HumLiquidacionViewSet(viewsets.ModelViewSet):
         except HumLiquidacion.DoesNotExist:
             return Response({'mensaje':'La liquidación no existe', 'codigo':15}, status=status.HTTP_400_BAD_REQUEST)     
         
-
     @action(detail=False, methods=["post"], url_path=r'aprobar',)
     def aprobar(self, request):
         try:
@@ -99,4 +98,54 @@ class HumLiquidacionViewSet(viewsets.ModelViewSet):
             else:
                 return Response({'mensaje':'Faltan parametros', 'codigo':1}, status=status.HTTP_400_BAD_REQUEST)
         except HumLiquidacion.DoesNotExist:
-            return Response({'mensaje':'La liquidación no existe', 'codigo':15}, status=status.HTTP_400_BAD_REQUEST)     
+            return Response({'mensaje':'La liquidación no existe', 'codigo':15}, status=status.HTTP_400_BAD_REQUEST)   
+
+    @action(detail=False, methods=["post"], url_path=r'desaprobar',)
+    def desaprobar(self, request):                     
+        raw = request.data
+        id = raw.get('id')
+        if id:
+            try:
+                liquidacion = HumLiquidacion.objects.get(pk=id)
+            except HumLiquidacion.DoesNotExist:
+                return Response({'mensaje':'La liquidacion no existe', 'codigo':15}, status=status.HTTP_400_BAD_REQUEST) 
+            if liquidacion.estado_aprobado == True:                                                                                                                
+                '''documentos = GenDocumento.objects.filter(programacion_detalle__programacion_id=id)
+                #Validar
+                for documento in documentos:
+                    if documento.afectado > 0:
+                        return Response({'mensaje':f'El documento {documento.id} ya tiene un egreso, no se puede desaprobar la programacion', 'codigo':15}, status=status.HTTP_400_BAD_REQUEST) 
+                    if documento.estado_contabilizado:
+                        return Response({'mensaje':f'El documento {documento.id} ya esta contabilizado, no se puede desaprobar la programacion', 'codigo':15}, status=status.HTTP_400_BAD_REQUEST) 
+                for documento in documentos:
+                    documento.estado_aprobado = False
+                    documento.pendiente = documento.total 
+                    documento.save()                        
+
+                    # Desafectar creditos
+                    documentos_detalles = GenDocumentoDetalle.objects.filter(documento_id=documento.id)
+                    for documento_detalle in documentos_detalles:
+                        if documento_detalle.credito:
+                            credito = documento_detalle.credito
+                            credito.abono -= documento_detalle.pago
+                            credito.saldo += documento_detalle.pago
+                            credito.cuota_actual -= 1
+                            credito.save()
+
+                    # Desafectar contratos
+                    contrato = documento.contrato
+                    if programacion.pago_tipo_id == 1:
+                        contrato.fecha_ultimo_pago = programacion.fecha_desde
+                    if programacion.pago_tipo_id == 2:
+                        contrato.fecha_ultimo_pago_prima = programacion.fecha_desde
+                    if programacion.pago_tipo_id == 3:
+                        contrato.fecha_ultimo_pago_cesantia = programacion.fecha_desde
+                    contrato.save()  '''                      
+                liquidacion.estado_aprobado = False
+                liquidacion.save()
+                return Response({'mensaje': 'Liquidacion desaprobada'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'mensaje':'La liquidacion no esta aprobada', 'codigo':1}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'mensaje':'Faltan parametros', 'codigo':1}, status=status.HTTP_400_BAD_REQUEST)
+          
