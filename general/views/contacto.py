dc96e6cf0964538e89648ac48fe518145938d504
sebas957 <baxtian957@hotmail.com>
2025-06-25 14:35:17 -0500

    ajustes contacto

--- general/views/contacto.py
+++ general/views/contacto.py
@@ -14,6 +14,7 @@ from utilidades.utilidades import Utilidades
 import base64
 import openpyxl
 import gc
+from rest_framework.pagination import PageNumberPagination
 
 class ContactoViewSet(viewsets.ModelViewSet):             
     permission_classes = [permissions.IsAuthenticated]
@@ -29,6 +30,11 @@ class ContactoViewSet(viewsets.ModelViewSet):
         return self.serializadores[serializador_parametro]
 
     def get_queryset(self):
+        page_size = self.request.query_params.get('page_size')
+        if page_size:
+            if page_size != '0':
+                self.pagination_class = PageNumberPagination
+                self.pagination_class.page_size = int(page_size)
         queryset = super().get_queryset()
         serializer_class = self.get_serializer_class()        
         select_related = getattr(serializer_class.Meta, 'select_related_fields', [])
