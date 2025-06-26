f4727c5b87c96c312e1551ee08da53989fe88306
sebas957 <baxtian957@hotmail.com>
2025-06-26 11:32:15 -0500

    ajustes precio

--- general/serializers/precio.py
+++ general/serializers/precio.py
@@ -2,32 +2,8 @@ from general.models.precio import GenPrecio
 from rest_framework import serializers
 
 
-class GenPrecioListaSerializador(serializers.ModelSerializer):          
+class GenPrecioSerializador(serializers.ModelSerializer):          
     class Meta:
         model = GenPrecio
         fields = ['id', 
                   'nombre']
-
-class GenPrecioSerializador(serializers.HyperlinkedModelSerializer):
-    
-    class Meta:
-        model = GenPrecio
-        fields = ['id', 'nombre', 'tipo', 'fecha_vence']
-
-    def to_representation(self, instance):
-        return {
-            'id': instance.id,            
-            'nombre': instance.nombre,
-            'tipo': instance.tipo,
-            'fecha_vence': instance.fecha_vence,
-        }    
-    
-class GenPrecioListaAutocompletarSerializador(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = GenPrecio 
-        
-    def to_representation(self, instance):
-        return {
-            'precio_id': instance.id,            
-            'precio_nombre': instance.nombre
-        }      
--- general/views/precio.py
+++ general/views/precio.py
@@ -1,10 +1,12 @@
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
 from general.models.precio import GenPrecio
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
-from general.serializers.precio import GenPrecioSerializador, GenPrecioListaSerializador
+from general.serializers.precio import GenPrecioSerializador
 from general.filters.precio import PrecioFilter
 from utilidades.excel_exportar import ExcelExportar
+from rest_framework.response import Response
 
 class PrecioViewSet(viewsets.ModelViewSet):
     queryset = GenPrecio.objects.all()
@@ -12,7 +14,7 @@ class PrecioViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_class = PrecioFilter 
     permission_classes = [permissions.IsAuthenticated]
-    serializadores = {'lista': GenPrecioListaSerializador}
+    serializadores = {'lista': GenPrecioSerializador}
 
     def get_serializer_class(self):
         serializador_parametro = self.request.query_params.get('serializador', None)
@@ -38,3 +40,18 @@ class PrecioViewSet(viewsets.ModelViewSet):
             exporter = ExcelExportar(serializer.data, sheet_name="precios", filename="precios.xlsx")
             return exporter.export()
         return super().list(request, *args, **kwargs)    
+    
+    @action(detail=False, methods=["get"], url_path=r'seleccionar')
+    def seleccionar_action(self, request):
+        limit = request.query_params.get('limit', 10)
+        nombre = request.query_params.get('nombre__icontains', None)
+        queryset = self.get_queryset()
+        if nombre:
+            queryset = queryset.filter(nombre__icontains=nombre)
+        try:
+            limit = int(limit)
+            queryset = queryset[:limit]
+        except ValueError:
+            pass    
+        serializer = self.get_serializer(queryset, many=True)        
+        return Response(serializer.data)    
