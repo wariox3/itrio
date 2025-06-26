c77f65bb1c8e3dc32a8dc95cf5e415647957acbf
sebas957 <baxtian957@hotmail.com>
2025-06-26 15:11:59 -0500

    Cuenta banco clase

--- general/serializers/cuenta_banco_clase.py
+++ general/serializers/cuenta_banco_clase.py
@@ -1,25 +1,9 @@
 from general.models.cuenta_banco_clase import GenCuentaBancoClase
 from rest_framework import serializers
 
-class GenCuentaBancoClaseSerializador(serializers.HyperlinkedModelSerializer):
+class GenCuentaBancoClaseSerializador(serializers.ModelSerializer):
     
     class Meta:
         model = GenCuentaBancoClase
         fields = ['id', 'nombre']
 
-    def to_representation(self, instance):
-        return {
-            'id': instance.id,            
-            'nombre': instance.nombre,
-        }  
-
-class GenCuentaBancoClaseListaAutocompletarSerializador(serializers.HyperlinkedModelSerializer):
-    
-    class Meta:
-        model = GenCuentaBancoClase
-
-    def to_representation(self, instance):
-        return {
-            'cuenta_banco_clase_id': instance.id,            
-            'cuenta_banco_clase_nombre': instance.nombre,
-        }       
