e67bea16eaa915789f46099b18569d69c4e326aa
sebas957 <baxtian957@hotmail.com>
2025-06-26 13:26:50 -0500

    filtrado en seleccioanr de item por inactivo

--- general/views/item.py
+++ general/views/item.py
@@ -70,9 +70,12 @@ class ItemViewSet(viewsets.ModelViewSet):
     def seleccionar_action(self, request):
         limit = request.query_params.get('limit', 10)
         nombre = request.query_params.get('nombre__icontains', None)
+        inactivo = request.query_params.get('inactivo', None)
         queryset = self.get_queryset()
         if nombre:
             queryset = queryset.filter(nombre__icontains=nombre)
+        if inactivo:
+            queryset = queryset.filter(inactivo=inactivo)
         try:
             limit = int(limit)
             queryset = queryset[:limit]
