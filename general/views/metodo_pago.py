003368abf34954f6fdfb2d8f1ca34f8930b94e46
sebas957 <baxtian957@hotmail.com>
2025-06-26 09:42:19 -0500

    ajustes plazo pago

--- general/views/metodo_pago.py
+++ general/views/metodo_pago.py
@@ -5,35 +5,32 @@ from general.models.metodo_pago import GenMetodoPago
 from general.serializers.metodo_pago import GenMetodoPagoSerializador, GenMetodoPagoListaSerializador
 from general.filters.metodo_pago import MetodoPagoFilter
 from utilidades.excel_exportar import ExcelExportar
+from rest_framework.response import Response
 
 class MetodoPagoViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_class = MetodoPagoFilter 
     queryset = GenMetodoPago.objects.all()   
-    serializadores = {'lista': GenMetodoPagoListaSerializador}
+    serializer_class = GenMetodoPagoSerializador
+    pagination_class = None
 
-    def get_serializer_class(self):
-        serializador_parametro = self.request.query_params.get('serializador', None)
-        if not serializador_parametro or serializador_parametro not in self.serializadores:
-            return GenMetodoPagoSerializador
-        return self.serializadores[serializador_parametro]
+
 
     def get_queryset(self):
-        queryset = super().get_queryset()
-        serializer_class = self.get_serializer_class()        
-        select_related = getattr(serializer_class.Meta, 'select_related_fields', [])
+        queryset = super().get_queryset()               
+        select_related = getattr(self.serializer_class.Meta, 'select_related_fields', [])
         if select_related:
             queryset = queryset.select_related(*select_related)        
-        campos = serializer_class.Meta.fields        
+        campos = self.serializer_class.Meta.fields        
         if campos and campos != '__all__':
-            queryset = queryset.only(*campos) 
+            queryset = queryset.only(*campos)     
         return queryset 
-
+    
     def list(self, request, *args, **kwargs):
-        if request.query_params.get('excel'):
-            queryset = self.filter_queryset(self.get_queryset())
-            serializer = self.get_serializer(queryset, many=True)
-            exporter = ExcelExportar(serializer.data, sheet_name="sedes", filename="sedes.xlsx")
-            return exporter.export()
-        return super().list(request, *args, **kwargs)    
+        queryset = self.filter_queryset(self.get_queryset())
+        limit = request.query_params.get('limit')
+        if limit and limit.isdigit():
+            queryset = queryset[:int(limit)]
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)   
