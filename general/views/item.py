55963cb74e942a29cca76fa1a71f123e2b4f0a6b
sebas957 <baxtian957@hotmail.com>
2025-06-26 11:47:12 -0500

    seleccionar item

--- general/views/item.py
+++ general/views/item.py
@@ -65,6 +65,21 @@ class ItemViewSet(viewsets.ModelViewSet):
         itemRespuesta = itemSerializador.data
         itemRespuesta['impuestos'] = itemImpuestosSerializador.data
         return Response({'item':itemRespuesta}, status=status.HTTP_200_OK)
+    
+    @action(detail=False, methods=["get"], url_path=r'seleccionar')
+    def seleccionar_action(self, request):
+        limit = request.query_params.get('limit', 10)
+        nombre = request.query_params.get('nombre__icontains', None)
+        queryset = self.get_queryset()
+        if nombre:
+            queryset = queryset.filter(nombre__icontains=nombre)
+        try:
+            limit = int(limit)
+            queryset = queryset[:limit]
+        except ValueError:
+            pass    
+        serializer = self.get_serializer(queryset, many=True)        
+        return Response(serializer.data)    
 
     def create(self, request):
         data = request.data
