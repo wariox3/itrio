c89eeca0af5b341e8336bdb465c93432e3218044
sebas957 <baxtian957@hotmail.com>
2025-06-26 11:20:45 -0500

    ajustes sede

--- general/serializers/sede.py
+++ general/serializers/sede.py
@@ -1,9 +1,8 @@
 from general.models.sede import GenSede
 from rest_framework import serializers
-from contabilidad.models.grupo import ConGrupo
 
 class GenSedeSerializador(serializers.HyperlinkedModelSerializer):
-    grupo__nombre = serializers.CharField(source='estado.nombre', read_only=True)
+    grupo__nombre = serializers.CharField(source='grupo.nombre', read_only=True)
     class Meta:
         model = GenSede
         fields = ['id', 'nombre', 'grupo__nombre']  
