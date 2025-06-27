d2853e9d09cea9e187997e73c509858e269d0477
sebas957 <baxtian957@hotmail.com>
2025-06-27 09:19:02 -0500

    Ajustes sede

--- general/serializers/sede.py
+++ general/serializers/sede.py
@@ -1,11 +1,12 @@
 from general.models.sede import GenSede
 from rest_framework import serializers
 
-class GenSedeSerializador(serializers.HyperlinkedModelSerializer):
-    grupo__nombre = serializers.CharField(source='grupo.nombre', read_only=True)
+class GenSedeSerializador(serializers.ModelSerializer):
+    grupo__nombre = serializers.SerializerMethodField()
     class Meta:
         model = GenSede
         fields = ['id', 'nombre', 'grupo__nombre']  
         select_related_fields = ['grupo']        
 
-  
+    def get_grupo__nombre(self, obj):
+        return obj.grupo.nombre if obj.grupo else '' 
