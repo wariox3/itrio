b6cad5e704ff9f187cb42dfdff364e6681e450a4
sebas957 <baxtian957@hotmail.com>
2025-07-01 14:21:52 -0500

    ajustes aporte_contrato

--- humano/serializers/aporte_contrato.py
+++ humano/serializers/aporte_contrato.py
@@ -22,7 +22,7 @@ class HumAporteContratoSerializador(serializers.ModelSerializer):
                   'cotizacion_pension_total',
                   'cotizacion_salud', 'cotizacion_riesgos', 'cotizacion_caja', 'cotizacion_sena', 'cotizacion_icbf', 'cotizacion_total',
                   'cotizacion_pension_empresa', 'cotizacion_pension_empleado', 'cotizacion_salud_empresa', 'cotizacion_salud_empleado', 'contrato__contacto__numero_identificacion',
-                  'contrato__contacto__nombre_corto']           
+                  'contrato__contacto__nombre_corto', 'contacto_id']           
         select_related_fields = ['contrato']
 
 #deprecated
