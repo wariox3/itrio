cd3d3e1cc7be226fcb0ef96aa60ae42f25c96d0a
sebas957 <baxtian957@hotmail.com>
2025-07-01 13:48:19 -0500

    Ajustes adicional

--- humano/serializers/adicional.py
+++ humano/serializers/adicional.py
@@ -4,7 +4,18 @@ from humano.models.contrato import HumContrato
 from humano.models.concepto import HumConcepto
 from humano.models.programacion import HumProgramacion
 
-class HumAdicionalSerializador(serializers.HyperlinkedModelSerializer):
+class HumAdicionalSerializador(serializers.ModelSerializer):
+    concepto__nombre = serializers.CharField(source='concepto.nombre', read_only=True)
+    contrato__contacto__numero_identificacion = serializers.CharField(source='contrato.contacto.numero_identificacion', read_only=True)
+    contrato__contacto__nombre_corto = serializers.CharField(source='contrato.contacto.nombre_corto', read_only=True)  
+
+    class Meta:
+        model = HumAdicional
+        fields = ['id', 'valor', 'horas', 'aplica_dia_laborado', 'detalle', 'concepto', 'contrato', 'programacion', 'permanente',
+                  'inactivo', 'concepto__nombre', 'contrato__contacto__numero_identificacion', 'contrato__contacto__nombre_corto']
+        select_related_fields = ['concepto','contrato']
+
+class HumAdicional1Serializador(serializers.HyperlinkedModelSerializer):
     concepto = serializers.PrimaryKeyRelatedField(queryset=HumConcepto.objects.all())
     contrato = serializers.PrimaryKeyRelatedField(queryset=HumContrato.objects.all())
     programacion = serializers.PrimaryKeyRelatedField(queryset=HumProgramacion.objects.all(), default=None, allow_null=True)
