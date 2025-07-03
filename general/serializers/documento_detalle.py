72d77f87087f9e4ba409cee5b2961ba91d64edef
sebas957 <baxtian957@hotmail.com>
2025-07-03 16:41:59 -0500

    ajustes

--- general/serializers/documento_detalle.py
+++ general/serializers/documento_detalle.py
@@ -23,7 +23,7 @@ class GenDocumentoDetalleAgregarDocumentoSerializador(serializers.ModelSerialize
     documento__documento__tipo__nombre = serializers.CharField(source='documento.documento_tipo.nombre', read_only=True)
     class Meta:
         model = GenDocumentoDetalle
-        fields = ['item_id', 'item__nombre', 'grupo_id',  'grupo__nombre', 'almacen_id', 'almacen__nombre',
+        fields = ['item', 'item__nombre', 'grupo',  'grupo__nombre', 'almacen', 'almacen__nombre',
                   'cantidad', 'precio', 'documento__numero' ,'documento__documento__tipo__nombre', 'documento__contacto__nombre_corto',
                   'documento__contacto__numero_identificacion']   
         select_related_fields = ['item','grupo', 'almacen', 'documento', 'documento_tipo', 'contacto']     
