6f9f51c4857e6575e28f1b6952757af0a963813a
sebas957 <baxtian957@hotmail.com>
2025-07-03 17:15:14 -0500

    endpoints para importar detalles base factura de venta y factura de compra

--- general/views/documento_detalle.py
+++ general/views/documento_detalle.py
@@ -3,10 +3,16 @@ from rest_framework.response import Response
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from general.models.documento_detalle import GenDocumentoDetalle
+from general.models.documento import GenDocumento
+from general.models.item import GenItem
+from inventario.models.almacen import InvAlmacen
 from general.serializers.documento_detalle import GenDocumentoDetalleSerializador, GenDocumentoDetalleInformeVentaSerializador, GenDocumentoDetalleAgregarDocumentoSerializador
 from general.filters.documento_detalle import DocumentoDetalleFilter
 from utilidades.excel_exportar import ExcelExportar
 from rest_framework.decorators import action
+import base64
+from io import BytesIO
+import openpyxl
 
 class DocumentoDetalleViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
@@ -79,3 +85,189 @@ class DocumentoDetalleViewSet(viewsets.ModelViewSet):
             pass    
         serializer = GenDocumentoDetalleAgregarDocumentoSerializador(queryset, many=True)        
         return Response(serializer.data)    
+
+
+    @action(detail=False, methods=["post"], url_path=r'importar_detalle_venta',)
+    def importar_detalle_venta(self, request):
+        raw = request.data
+        documento_id = raw.get('documento_id')
+        archivo_base64 = raw.get('archivo_base64')
+        if documento_id and archivo_base64:
+            try:
+                documento = GenDocumento.objects.get(pk=documento_id)
+            except GenDocumento.DoesNotExist:
+                return Response({'mensaje':'El documento no existe', 'codigo':15}, status=status.HTTP_400_BAD_REQUEST)
+            try:
+                archivo_data = base64.b64decode(archivo_base64)
+                archivo = BytesIO(archivo_data)
+                wb = openpyxl.load_workbook(archivo)
+                sheet = wb.active         
+            except Exception as e:     
+                return Response({'mensaje':'Error procesando el archivo', 'codigo':15}, status=status.HTTP_400_BAD_REQUEST)  
+            
+            data_documento_detalle = []
+            errores = False
+            errores_datos = []
+            registros_importados = 0
+            if documento.documento_tipo_id in [1]:
+                for i, row in enumerate(sheet.iter_rows(min_row=2, values_only=True), start=2):
+                    if len(row) == 4:
+                        data = {
+                            'item': row[0],
+                            'almacen':row[1],
+                            'cantidad': row[2] if row[2] is not None else 0,
+                            'precio': row[3] if row[3] is not None else 0,              
+                        }                    
+                        if not data['item']:
+                            error_dato = {
+                                'fila': i,
+                                'Mensaje': 'Debe digitar el código del item'
+                            }
+                            errores_datos.append(error_dato)
+                            errores = True
+                        else:
+                            item = GenItem.objects.filter(id=data['item']).first()
+                            if item is None:
+                                error_dato = {
+                                    'fila': i,
+                                    'Mensaje': f'El item {data["item"]} no existe'
+                                }
+                                errores_datos.append(error_dato)
+                                errores = True
+                            else:
+                                data['item_id'] = item.id
+
+                        if not data['almacen']:
+                            error_dato = {
+                                'fila': i,
+                                'Mensaje': 'Debe digitar el código del almacen'
+                            }
+                            errores_datos.append(error_dato)
+                            errores = True  
+                        else:
+                            almacen = InvAlmacen.objects.filter(id=data['almacen']).first()
+                            if almacen is None:
+                                error_dato = {
+                                    'fila': i,
+                                    'Mensaje': f'El almacen con código {data["almacen"]} no existe'
+                                }
+                                errores_datos.append(error_dato)
+                                errores = True
+                            else:
+                                data['almacen_id'] = almacen.id                                 
+                        data_documento_detalle.append(data) 
+                    else:
+                        error_dato = {
+                            'fila': i,
+                            'Mensaje': f'La linea no tiene 4 columnas'
+                        }
+                        errores_datos.append(error_dato)
+                        errores = True
+            if errores == False:
+                for detalle in data_documento_detalle:
+                    GenDocumentoDetalle.objects.create(
+                        documento=documento,
+                        item_id=detalle['item_id'],
+                        almacen_id=detalle['almacen_id'],
+                        cantidad=detalle['cantidad'],
+                        precio=detalle['precio']
+                    )
+                    registros_importados += 1
+                return Response({'registros_importados': registros_importados}, status=status.HTTP_200_OK)
+            else:
+                return Response({'errores': True, 'errores_datos': errores_datos}, status=status.HTTP_400_BAD_REQUEST)       
+        else:
+            return Response({'mensaje':'Faltan parametros', 'codigo':1}, status=status.HTTP_400_BAD_REQUEST)
+        
+    @action(detail=False, methods=["post"], url_path=r'importar_detalle_compra',)
+    def importar_detalle_compra(self, request):
+        raw = request.data
+        documento_id = raw.get('documento_id')
+        archivo_base64 = raw.get('archivo_base64')
+        if documento_id and archivo_base64:
+            try:
+                documento = GenDocumento.objects.get(pk=documento_id)
+            except GenDocumento.DoesNotExist:
+                return Response({'mensaje':'El documento no existe', 'codigo':15}, status=status.HTTP_400_BAD_REQUEST)
+            try:
+                archivo_data = base64.b64decode(archivo_base64)
+                archivo = BytesIO(archivo_data)
+                wb = openpyxl.load_workbook(archivo)
+                sheet = wb.active         
+            except Exception as e:     
+                return Response({'mensaje':'Error procesando el archivo', 'codigo':15}, status=status.HTTP_400_BAD_REQUEST)  
+            
+            data_documento_detalle = []
+            errores = False
+            errores_datos = []
+            registros_importados = 0
+            if documento.documento_tipo_id in [5]:
+                for i, row in enumerate(sheet.iter_rows(min_row=2, values_only=True), start=2):
+                    if len(row) == 4:
+                        data = {
+                            'item': row[0],
+                            'grupo':row[1],
+                            'almacen': row[2],
+                            'cantidad': row[3] if row[3] is not None else 0,
+                            'precio': row[4] if row[4] is not None else 0,              
+                        }                    
+                        if not data['item']:
+                            error_dato = {
+                                'fila': i,
+                                'Mensaje': 'Debe digitar el código del item'
+                            }
+                            errores_datos.append(error_dato)
+                            errores = True
+                        else:
+                            item = GenItem.objects.filter(id=data['item']).first()
+                            if item is None:
+                                error_dato = {
+                                    'fila': i,
+                                    'Mensaje': f'El item {data["item"]} no existe'
+                                }
+                                errores_datos.append(error_dato)
+                                errores = True
+                            else:
+                                data['item_id'] = item.id
+
+                        if not data['almacen']:
+                            error_dato = {
+                                'fila': i,
+                                'Mensaje': 'Debe digitar el código del almacen'
+                            }
+                            errores_datos.append(error_dato)
+                            errores = True  
+                        else:
+                            almacen = InvAlmacen.objects.filter(id=data['almacen']).first()
+                            if almacen is None:
+                                error_dato = {
+                                    'fila': i,
+                                    'Mensaje': f'El almacen con código {data["almacen"]} no existe'
+                                }
+                                errores_datos.append(error_dato)
+                                errores = True
+                            else:
+                                data['almacen_id'] = almacen.id                                 
+                        data_documento_detalle.append(data) 
+                    else:
+                        error_dato = {
+                            'fila': i,
+                            'Mensaje': f'La linea no tiene 4 columnas'
+                        }
+                        errores_datos.append(error_dato)
+                        errores = True
+            if errores == False:
+                for detalle in data_documento_detalle:
+                    GenDocumentoDetalle.objects.create(
+                        documento=documento,
+                        item_id=detalle['item_id'],
+                        almacen_id=detalle['almacen_id'],
+                        cantidad=detalle['cantidad'],
+                        precio=detalle['precio']
+                    )
+                    registros_importados += 1
+                return Response({'registros_importados': registros_importados}, status=status.HTTP_200_OK)
+            else:
+                return Response({'errores': True, 'errores_datos': errores_datos}, status=status.HTTP_400_BAD_REQUEST)       
+        else:
+            return Response({'mensaje':'Faltan parametros', 'codigo':1}, status=status.HTTP_400_BAD_REQUEST)
