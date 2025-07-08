c5abb41b72a1b8d3755a5ae90662549db9485ec7
sebas957 <baxtian957@hotmail.com>
2025-07-08 08:21:42 -0500

    Ajustes seleccionar impuesto

--- general/views/impuesto.py
+++ general/views/impuesto.py
@@ -23,9 +23,12 @@ class ImpuestoViewSet(viewsets.ModelViewSet):
     def seleccionar_action(self, request):
         limit = request.query_params.get('limit', 10)
         nombre = request.query_params.get('nombre__icontains', None)
+        venta = request.query_params.get('venta', None)
         queryset = self.get_queryset()
         if nombre:
             queryset = queryset.filter(nombre__icontains=nombre)
+        if venta:
+            queryset = queryset.filter(venta=venta)
         try:
             limit = int(limit)
             queryset = queryset[:limit]
