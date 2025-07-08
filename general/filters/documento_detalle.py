74b8a6338f4c9dd63c94c6e83d02ab7cacbd8627
sebas957 <baxtian957@hotmail.com>
2025-07-08 09:25:29 -0500

    ajustes filtrado documento detalle

--- general/filters/documento_detalle.py
+++ general/filters/documento_detalle.py
@@ -2,10 +2,12 @@ import django_filters
 from general.models.documento_detalle import GenDocumentoDetalle
 from django_filters import NumberFilter
 
-class DocumentoDetalleFilter(django_filters.FilterSet):     
+class DocumentoDetalleFilter(django_filters.FilterSet):
+    item__nombre = django_filters.CharFilter(field_name='item__nombre', lookup_expr='icontains')     
     class Meta:
         model = GenDocumentoDetalle        
         fields = {'id': ['exact', 'lte'],
                   'documento__estado_aprobado':['exact'],
                   'documento__documento_tipo__venta':['exact'],
-                  'documento_id': ['exact']}
+                  'documento_id': ['exact'],
+                  'item__nombre':['icontains'],}
