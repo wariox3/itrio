2372543e8b903e5b43426ff2783cef9e87f43b55
MarioA <maestradaz3@gmail.com>
2025-07-08 16:45:37 -0500

    feat: nuevo operador a documento detalle

--- general/filters/documento_detalle.py
+++ general/filters/documento_detalle.py
@@ -8,11 +8,14 @@ class DocumentoDetalleFilter(django_filters.FilterSet):
     documento__contacto__numero_identificacion = django_filters.CharFilter(field_name='documento__contacto__numero_identificacion', lookup_expr='icontains')    
     class Meta:
         model = GenDocumentoDetalle        
-        fields = {'id': ['exact', 'lte'],
-                  'documento__estado_aprobado':['exact'],
-                  'documento__numero':['exact', 'icontains'],
-                  'documento__contacto__nombre_corto':['exact', 'icontains'],
-                  'documento__contacto__numero_identificacion':['exact', 'icontains'],
-                  'documento__documento_tipo__venta':['exact'],
-                  'documento_id': ['exact'],
-                  'item__nombre':['icontains'],}
+        fields = {
+                    'id': ['exact', 'lte'],
+                    'operacion_inventario': ['exact', 'exclude'],
+                    'documento__estado_aprobado':['exact'],
+                    'documento__numero':['exact', 'icontains'],
+                    'documento__contacto__nombre_corto':['exact', 'icontains'],
+                    'documento__contacto__numero_identificacion':['exact', 'icontains'],
+                    'documento__documento_tipo__venta':['exact'],
+                    'documento_id': ['exact'],
+                    'item__nombre':['icontains'],
+                }
