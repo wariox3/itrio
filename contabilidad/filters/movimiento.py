209f1381d481b11d82808077cb09b7969b6ec66d
sebas957 <baxtian957@hotmail.com>
2025-07-10 08:40:40 -0500

    ajustes filtro movimiento contabilidad

--- contabilidad/filters/movimiento.py
+++ contabilidad/filters/movimiento.py
@@ -1,8 +1,10 @@
 import django_filters
 from contabilidad.models.movimiento import ConMovimiento
 
-class MovimientoFilter(django_filters.FilterSet):    
+class MovimientoFilter(django_filters.FilterSet):   
+    cuenta__codigo = django_filters.CharFilter(field_name='cuenta__codigo', lookup_expr='icontains') 
     class Meta:
         model = ConMovimiento
         fields = {'id': ['exact'],
-                  'documento_id': ['exact']}
+                  'documento_id': ['exact'],
+                  'cuenta__codigo': ['icontains']}
