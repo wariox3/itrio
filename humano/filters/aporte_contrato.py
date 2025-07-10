04243cfe69ad1cdc0d4351acaea1145c53ab7a02
sebas957 <baxtian957@hotmail.com>
2025-07-10 09:43:23 -0500

    ajustes

--- humano/filters/aporte_contrato.py
+++ humano/filters/aporte_contrato.py
@@ -3,7 +3,7 @@ from humano.models.aporte_contrato import HumAporteContrato
 
 class AporteContratoFilter(django_filters.FilterSet):
     contrato__contacto__nombre_corto = django_filters.CharFilter(field_name='contrato__contacto__nombre_corto', lookup_expr='icontains')
-    contrato__contacto_id =  django_filters.NumberFilter(field_name='contrato_contacto_id', lookup_expr='exact')
+    contrato__contacto_id =  django_filters.NumberFilter(field_name='contrato__contacto_id', lookup_expr='exact')
     class Meta:
         model = HumAporteContrato        
         fields = {'id': ['exact', 'lte'],
