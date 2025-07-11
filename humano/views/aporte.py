14cde53ae516340f589960846f98816a332aaaea
sebas957 <baxtian957@hotmail.com>
2025-07-11 09:36:55 -0500

    Ajustes excel aportes

--- humano/views/aporte.py
+++ humano/views/aporte.py
@@ -32,6 +32,7 @@ import io
 from datetime import datetime
 from decimal import Decimal
 from django.db import transaction
+from utilidades.excel_exportar import ExcelExportar
 
 def calcular_porcentaje_fondo(salario_minimo, base_cotizacion):
     salarios_minimos = base_cotizacion / salario_minimo
@@ -56,6 +57,42 @@ class HumAporteViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]  
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_class = AporteFilter       
+
+    def get_serializer_class(self):
+        serializador_parametro = self.request.query_params.get('serializador', None)
+        if not serializador_parametro or serializador_parametro not in self.serializadores:
+            return HumAporteSerializador
+        return self.serializadores[serializador_parametro]    
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        serializer_class = self.get_serializer_class()        
+        select_related = getattr(serializer_class.Meta, 'select_related_fields', [])
+        if select_related:
+            queryset = queryset.select_related(*select_related)        
+        campos = serializer_class.Meta.fields        
+        if campos and campos != '__all__':
+            queryset = queryset.only(*campos)
+        return queryset            
+
+    def list(self, request, *args, **kwargs):
+        if request.query_params.get('excel'):
+            queryset = self.filter_queryset(self.get_queryset())
+            serializer = self.get_serializer(queryset, many=True)
+            titulo = 'Aportes'
+            nombre_hoja = "aportes"
+            nombre_archivo = "aportes.xlsx"
+            if request.query_params.get('excel_masivo'):
+                exporter = ExcelExportar(serializer.data, nombre_hoja, nombre_archivo)
+                return exporter.exportar() 
+            elif request.query_params.get('excel_informe'): 
+                serializador_parametro = self.request.query_params.get('serializador', None)                 
+                exporter = ExcelExportar(serializer.data, nombre_hoja, nombre_archivo, titulo)
+                return exporter.exportar_informe()                    
+            else:
+                exporter = ExcelExportar(serializer.data, nombre_hoja, nombre_archivo)
+                return exporter.exportar_estilo()            
+        return super().list(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         anio = serializer.validated_data.get('anio')
