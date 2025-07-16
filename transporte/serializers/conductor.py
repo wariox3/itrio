2cc1249edc8b86cebf8b44784af8510efd7ae4b2
sebas957 <baxtian957@hotmail.com>
2025-07-16 11:08:50 -0500

    Ajustes conductor

--- transporte/serializers/conductor.py
+++ transporte/serializers/conductor.py
@@ -9,7 +9,7 @@ class TteConductorSerializador(serializers.ModelSerializer):
     rh__codigo = serializers.CharField(source='rh.codigo', read_only=True) 
     class Meta:
         model = TteConductor
-        fields = ['id', 'identificacion', 'identificacion__nombre', 'ciudad' ,'ciudad__nombre' ,'ciudad__estado__nombre', 'rh', 'rh__nombre', 'rh__codigo',
+        fields = ['id', 'identificacion', 'identificacion__nombre', 'ciudad' ,'ciudad__nombre' ,'ciudad__estado__nombre', 'rh', 'rh__nombre', 'rh__codigo', 'digito_verificacion',
                   'numero_identificacion', 'nombre_corto', 'nombre1', 'nombre2', 'apellido1', 'apellido2',
                   'direccion', 'barrio', 'telefono', 'celular', 'correo', 'numero_licencia', 'categoria_licencia', 'fecha_nacimiento',
                   'fecha_vence_licencia', 'fecha_expedicion_licencia', 'fecha_ingreso', 'fecha_retiro',
