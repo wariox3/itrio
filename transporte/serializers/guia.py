147186c802b2dee7112882195002a848a1bf84d1
sebas957 <baxtian957@hotmail.com>
2025-07-17 15:20:47 -0500

    Ajustes guia

--- transporte/serializers/guia.py
+++ transporte/serializers/guia.py
@@ -2,13 +2,14 @@ from rest_framework import serializers
 from transporte.models.guia import TteGuia
 
 
-class TteGuiaSerializador(serializers.HyperlinkedModelSerializer):    
+class TteGuiaSerializador(serializers.ModelSerializer):   
+
     class Meta:
         model = TteGuia
-        fields = ['id']
-
-    def to_representation(self, instance):        
-        return {
-            'id': instance.id,            
-        }
+        fields = ['id', 'fecha', 'destinatario', 'ciudad_destino', 'ciudad_origen', 'cliente', 'contacto', 'empaque',
+                  'servicio', 'producto', 'ruta', 'zona', 'operacion_cargo', 'operacion_ingreso', 'unidades', 'peso',
+                  'volumen', 'peso_facturado', 'declara', 'flete', 'manejo', 'recaudo', 'cobro_entrega', 'contenido_verificado',
+                  'mercancia_peligrosa', 'requiere_cita', 'comentario']
+        select_related_fields = ['contacto', 'destinatario', 'ciudad_destino', 'ciudad_origen', 'cliente', 'empaque'
+                                 'servicio', 'producto', 'ruta', 'zona', 'operacion_cargo', 'operacion_ingreso']  
     
