b68ac6bffd8ab77daf041d049204642afce3e17f
MarioA <maestradaz3@gmail.com>
2025-07-25 17:12:40 -0500

    feat: vida token

--- itrioapp/settings.py
+++ itrioapp/settings.py
@@ -186,7 +186,7 @@ SIMPLE_JWT = {
     #'ACCESS_TOKEN_LIFETIME': timedelta(hours=12),
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
     #'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=3),
 }
 
 AUTH_USER_MODEL = 'seguridad.User'
