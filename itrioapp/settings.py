16c31801060cc8781018bb4ed76b88624ab1053a
MarioA <maestradaz3@gmail.com>
2025-07-25 16:38:43 -0500

    feat: vida de los token

--- itrioapp/settings.py
+++ itrioapp/settings.py
@@ -185,7 +185,8 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = { 
     #'ACCESS_TOKEN_LIFETIME': timedelta(hours=12),
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    #'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=1),
 }
 
 AUTH_USER_MODEL = 'seguridad.User'
