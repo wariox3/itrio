01477a21f0d09ab3dd8d4eb40a1992e0a8bf5464
MarioA <maestradaz3@gmail.com>
2025-07-31 08:35:09 -0500

    feat: serializadores complemento

---
+++ general/filters/complemento.py
@@ -0,0 +1,9 @@
+import django_filters
+from general.models.complemento import GenComplemento
+
+class ComplementoFilter(django_filters.FilterSet):    
+    class Meta:
+        model = GenComplemento        
+        fields = {
+            'id': ['exact', 'lte']
+        }
--- general/views/complemento.py
+++ general/views/complemento.py
@@ -3,12 +3,42 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from general.models.complemento import GenComplemento
 from general.serializers.complemento import GenComplementoSerializador
+from general.filters.complemento import ComplementoFilter
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from utilidades.holmio import Holmio
 
 class ComplementoViewSet(viewsets.ModelViewSet):
-    queryset = GenComplemento.objects.all()
-    serializer_class = GenComplementoSerializador
     permission_classes = [permissions.IsAuthenticated]
+    queryset = GenComplemento.objects.all()
+    serializer_class = GenComplementoSerializador    
+    filter_backends = [DjangoFilterBackend, OrderingFilter]    
+    filterset_class = ComplementoFilter 
+    serializadores = {
+        'lista': GenComplementoSerializador
+    }
+
+    def get_serializer_class(self):
+        serializador_parametro = self.request.query_params.get('serializador', None)
+        if not serializador_parametro or serializador_parametro not in self.serializadores:
+            return GenComplementoSerializador
+        return self.serializadores[serializador_parametro]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        serializer_class = self.get_serializer_class()        
+        select_related = getattr(serializer_class.Meta, 'select_related_fields', [])
+        if select_related:
+            queryset = queryset.select_related(*select_related)        
+        campos = serializer_class.Meta.fields        
+        if campos and campos != '__all__':
+            queryset = queryset.only(*campos) 
+        return queryset 
+
+    def list(self, request, *args, **kwargs):
+        if request.query_params.get('lista', '').lower() == 'true':
+            self.pagination_class = None
+        return super().list(request, *args, **kwargs)
 
     @action(detail=False, methods=["post"], url_path=r'validar',)
     def validar_action(self, request):             
