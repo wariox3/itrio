9dd450a7e8200fa98261ddd6159e3e9ec0952211
sebas957 <baxtian957@hotmail.com>
2025-08-06 15:18:14 -0500

    Ajustes

---
+++ transporte/filters/guia.py
@@ -0,0 +1,9 @@
+import django_filters
+from transporte.models.guia import TteGuia
+
+class GuiaFilter(django_filters.FilterSet):    
+    
+    class Meta:
+        model = TteGuia
+        fields = {'id': ['exact']                  
+                  }
--- transporte/views/guia.py
+++ transporte/views/guia.py
@@ -3,14 +3,47 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from transporte.models.guia import TteGuia
 from transporte.serializers.guia import TteGuiaSerializador
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from utilidades.excel_exportar import ExcelExportar
+from transporte.filters.guia import GuiaFilter
 
 
 class GuiaViewSet(viewsets.ModelViewSet):
     queryset = TteGuia.objects.all()
     serializer_class = TteGuiaSerializador
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_class = GuiaFilter 
+    serializadores = {
+    }
 
-    @action(detail=False, methods=["post"], url_path=r'importar_ruteo',)
-    def lista(self, request):
-        raw = request.data
-        return Response({'mensaje':'hola mundo'}, status=status.HTTP_200_OK)   
+    def get_serializer_class(self):
+        serializador_parametro = self.request.query_params.get('serializador', None)
+        if not serializador_parametro or serializador_parametro not in self.serializadores:
+            return TteGuiaSerializador
+        return self.serializadores[serializador_parametro]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        serializer_class = self.get_serializer_class()        
+        select_related = getattr(serializer_class.Meta, 'select_related_fields', [])
+        if select_related:
+            queryset = queryset.select_related(*select_related)        
+        campos = serializer_class.Meta.fields        
+        if campos and campos != '__all__':
+            queryset = queryset.only(*campos) 
+        return queryset 
+
+    def list(self, request, *args, **kwargs):
+        if request.query_params.get('lista_completa', '').lower() == 'true':
+            self.pagination_class = None
+        if request.query_params.get('excel') or request.query_params.get('excel_masivo'):
+            queryset = self.filter_queryset(self.get_queryset())
+            serializer = self.get_serializer(queryset, many=True)
+            exporter = ExcelExportar(serializer.data, nombre_hoja="visitas", nombre_archivo="visitas.xlsx", titulo="Visitas")
+            if request.query_params.get('excel'):
+                return exporter.exportar_estilo()
+            if request.query_params.get('excel_masivo'):
+                return exporter.exportar()
+        return super().list(request, *args, **kwargs)
