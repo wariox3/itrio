a63ef412b45e990302ae01872bd6141f1b49ac7b
sebas957 <baxtian957@hotmail.com>
2025-08-11 15:42:18 -0500

    Se ajusta serializador de operaciones apra retornar ciudad

--- transporte/serializers/operacion.py
+++ transporte/serializers/operacion.py
@@ -2,9 +2,11 @@ from rest_framework import serializers
 from transporte.models.operacion import TteOperacion
 
 class TteOperacionSerializador(serializers.ModelSerializer):    
+    ciudad__nombre = serializers.CharField(source='ciudad.nombre', read_only=True, allow_null=True, default=None)
     class Meta:
         model = TteOperacion
-        fields = ['id', 'nombre', 'ciudad']
+        fields = ['id', 'nombre', 'ciudad', 'ciudad__nombre']
+        select_related_fields = ['ciudad']          
 
 class TteOpercionSeleccionarSerializador(serializers.ModelSerializer):    
     class Meta:
