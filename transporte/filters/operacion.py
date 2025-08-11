9496c2e723ec4ec6d904d6c41f53905d9ae029c2
sebas957 <baxtian957@hotmail.com>
2025-08-11 16:04:19 -0500

    Filtrado de operacion por nombre

---
+++ transporte/filters/operacion.py
@@ -0,0 +1,10 @@
+import django_filters
+from transporte.models.operacion import TteOperacion
+
+class OperacionFilter(django_filters.FilterSet):    
+    ciudad__nombre = django_filters.CharFilter(field_name='ciudad__nombre', lookup_expr='icontains')
+    class Meta:
+        model = TteOperacion
+        fields = {'id': ['exact'],
+                  'ciudad__nombre': ['exact', 'icontains'],                  
+                  }
