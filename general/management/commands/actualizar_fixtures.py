c8be3b62d7e8053549c77e60cd34d2a221d8b568
MarioA <maestradaz3@gmail.com>
2025-08-11 11:07:11 -0500

    feat: pausar modelos de fixture

--- general/management/commands/actualizar_fixtures.py
+++ general/management/commands/actualizar_fixtures.py
@@ -34,36 +34,40 @@ class Command(BaseCommand):
                     creados = 0
                     nombre_archivo = os.path.basename(json_file)
                     data = json.load(file)                                 
-                    Model = apps.get_model(data['aplicacion'], data['modelo'])           
-                    for item in data['datos']:                                           
-                        pk = item.get('pk')                
-                        campos = item.get('campos', {})                                                                    
-                        modelo_campos = [field.name for field in Model._meta.get_fields()]
-                        filtered_fields = {}
-                        for key, value in campos.items():
-                            if key in modelo_campos:
-                                field = Model._meta.get_field(key)
-                                if isinstance(field, models.ForeignKey):
-                                    related_model = field.related_model
-                                    try:
-                                        related_instance = related_model.objects.get(pk=value)
-                                        filtered_fields[key] = related_instance
-                                    except related_model.DoesNotExist:
-                                        self.stdout.write(self.style.ERROR(f'El objeto relacionado {related_model} con pk={value} no existe.'))
-                                        continue
-                                else:
-                                    filtered_fields[key] = value
-                        instance, created = Model.objects.update_or_create(pk=pk, defaults=filtered_fields)                    
-                        if created:
-                            creados += 1
-                        else:
-                            actualizados += 1
-                    actualizar_secuencia = data['actualizar_secuencia']
-                    if actualizar_secuencia:
-                        modelo_nativo = data['modelo_nativo']
-                        cursor = connection.cursor()
-                        cursor.execute(f"SELECT setval(pg_get_serial_sequence('{modelo_nativo}', 'id'), (SELECT MAX(id) FROM {modelo_nativo}));")
-                        cursor.close()
-                    self.stdout.write(self.style.SUCCESS(f'Registros creados {creados} actualizados {actualizados} modelo {nombre_archivo}'))
+                    pausa = data.get('pausa', False)
+                    if pausa == False:
+                        Model = apps.get_model(data['aplicacion'], data['modelo'])           
+                        for item in data['datos']:                                           
+                            pk = item.get('pk')                
+                            campos = item.get('campos', {})                                                                    
+                            modelo_campos = [field.name for field in Model._meta.get_fields()]
+                            filtered_fields = {}
+                            for key, value in campos.items():
+                                if key in modelo_campos:
+                                    field = Model._meta.get_field(key)
+                                    if isinstance(field, models.ForeignKey):
+                                        related_model = field.related_model
+                                        try:
+                                            related_instance = related_model.objects.get(pk=value)
+                                            filtered_fields[key] = related_instance
+                                        except related_model.DoesNotExist:
+                                            self.stdout.write(self.style.ERROR(f'El objeto relacionado {related_model} con pk={value} no existe.'))
+                                            continue
+                                    else:
+                                        filtered_fields[key] = value
+                            instance, created = Model.objects.update_or_create(pk=pk, defaults=filtered_fields)                    
+                            if created:
+                                creados += 1
+                            else:
+                                actualizados += 1
+                        actualizar_secuencia = data['actualizar_secuencia']
+                        if actualizar_secuencia:
+                            modelo_nativo = data['modelo_nativo']
+                            cursor = connection.cursor()
+                            cursor.execute(f"SELECT setval(pg_get_serial_sequence('{modelo_nativo}', 'id'), (SELECT MAX(id) FROM {modelo_nativo}));")
+                            cursor.close()
+                        self.stdout.write(self.style.SUCCESS(f'Registros creados {creados} actualizados {actualizados} modelo {nombre_archivo}'))
+                    else:
+                        self.stdout.write(self.style.SUCCESS(f'Pausado modelo {nombre_archivo}'))
             except FileNotFoundError:
                 self.stdout.write(self.style.ERROR('El archivo JSON no existe'))
