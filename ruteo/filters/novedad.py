869b3cf83ee6e748d7fde91b1ae67a7ea2fd3b67
sebas957 <baxtian957@hotmail.com>
2025-08-12 13:45:29 -0500

    filtrado novedades

--- ruteo/filters/novedad.py
+++ ruteo/filters/novedad.py
@@ -1,11 +1,13 @@
 import django_filters
 from ruteo.models.novedad import RutNovedad
 
-class NovedadFilter(django_filters.FilterSet):    
+class NovedadFilter(django_filters.FilterSet):   
+    visita__numero = django_filters.NumberFilter(field_name='visita__numero', lookup_expr='exact') 
     class Meta:
         model = RutNovedad
         fields = {'id': ['exact'],
                   'estado_solucion': ['exact'], 
                   'nuevo_complemento': ['exact'],
                   'visita_id': ['exact'],
+                  'visita__numero': ['exact'],
                   }
--- ruteo/serializers/novedad.py
+++ ruteo/serializers/novedad.py
@@ -3,10 +3,11 @@ from ruteo.models.novedad import RutNovedad
 
 class RutNovedadSerializador(serializers.ModelSerializer):   
     novedad_tipo__nombre = serializers.CharField(source='novedad_tipo.nombre', read_only=True, allow_null=True, default=None)         
+    visita__numero = serializers.IntegerField(source='visita.numero', read_only=True, allow_null=True, default=None)    
     
     class Meta:
         model = RutNovedad
-        fields = ['id', 'fecha', 'fecha_solucion', 'fecha_registro', 'descripcion', 'solucion', 'estado_solucion', 'visita', 'novedad_tipo',
+        fields = ['id', 'fecha', 'fecha_solucion', 'fecha_registro', 'descripcion', 'solucion', 'estado_solucion', 'visita', 'visita__numero' ,'novedad_tipo',
                   'novedad_tipo__nombre', 'nuevo_complemento']
         select_related_fields = ['novedad_tipo']    
     
