3607b919db1929906d52305be1018f76fe6248ca
sebas957 <baxtian957@hotmail.com>
2025-08-21 15:02:34 -0500

    Ajustes filtrado contenedor

--- contenedor/filters/usuario_contenedor.py
+++ contenedor/filters/usuario_contenedor.py
@@ -3,10 +3,14 @@ from contenedor.models import UsuarioContenedor
 
 class UsuarioContenedorFilter(django_filters.FilterSet):   
     contenedor__nombre = django_filters.CharFilter(field_name='contenedor__nombre', lookup_expr='icontains')
+    contenedor__reddoc = django_filters.BooleanFilter(field_name='contenedor__reddoc', lookup_expr='exact')    
+    contenedor__ruteo = django_filters.BooleanFilter(field_name='contenedor__ruteo', lookup_expr='exact')    
     class Meta:
         model = UsuarioContenedor
         fields = {
             'id': ['exact', 'lte'],
             'usuario_id': ['exact'],
-            'contenedor__nombre': ['icontains']       
+            'contenedor__nombre': ['icontains'],    
+            'contenedor__reddoc': ['exact'],
+            'contenedor__ruteo': ['exact'],
         }
