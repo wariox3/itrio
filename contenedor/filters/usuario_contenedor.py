c0926e09b4b78cb4fa0ad46de6ccac9daf307887
sebas957 <baxtian957@hotmail.com>
2025-08-21 14:44:50 -0500

    filtro por nombre de contenedor

--- contenedor/filters/usuario_contenedor.py
+++ contenedor/filters/usuario_contenedor.py
@@ -2,10 +2,11 @@ import django_filters
 from contenedor.models import UsuarioContenedor
 
 class UsuarioContenedorFilter(django_filters.FilterSet):   
-         
+    contenedor__nombre = django_filters.CharFilter(field_name='contenedor__nombre', lookup_expr='icontains')
     class Meta:
         model = UsuarioContenedor
         fields = {
             'id': ['exact', 'lte'],
-            'usuario_id': ['exact']       
+            'usuario_id': ['exact'],
+            'contenedor__nombre': ['icontains']       
         }
