84364fb057d83771a088b0eac81ca89f1e50f8ad
sebas957 <baxtian957@hotmail.com>
2025-08-22 09:59:06 -0500

    Ajustes modelo despacho

--- transporte/models/despacho.py
+++ transporte/models/despacho.py
@@ -39,7 +39,7 @@ class TteDespacho(models.Model):
     ciudad_destino = models.ForeignKey(GenCiudad, on_delete=models.PROTECT, related_name='despachos_ciudad_destino_rel')
     vehiculo = models.ForeignKey(TteVehiculo, on_delete=models.PROTECT, related_name='despachos_vehiculo_rel')
     remolque = models.ForeignKey(TteVehiculo, null=True, on_delete=models.PROTECT, related_name='despachos_remolque_rel')
-    ruta = models.ForeignKey(TteRuta, on_delete=models.PROTECT, related_name='despachos_ruta_rel')
+    ruta = models.ForeignKey(TteRuta, null=True, on_delete=models.PROTECT, related_name='despachos_ruta_rel')
 
     class Meta:
         db_table = "tte_despacho"
