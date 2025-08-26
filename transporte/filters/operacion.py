ec4e1a92e1557e4f965c1055ed675397db904dc9
sebas957 <baxtian957@hotmail.com>
2025-08-26 08:43:42 -0500

    Ajustes operacion

--- transporte/filters/operacion.py
+++ transporte/filters/operacion.py
@@ -8,6 +8,7 @@ class OperacionFilter(django_filters.FilterSet):
         fields = {'id': ['exact'],
                   'ciudad__nombre': ['exact', 'icontains'],    
                   'ciudad_id': ['exact'],              
+                  'ciudad': ['exact'],        
                   'nombre': ['exact', 'icontains'],              
                   }
         
