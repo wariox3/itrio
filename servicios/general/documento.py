9bfd34e629f35749cd763ee4cd24b88a6302e60c
wariox3 <maestradaz3@gmail.com>
2025-08-26 18:38:05 -0500

    feat: calcular cufe

--- servicios/general/documento.py
+++ servicios/general/documento.py
@@ -4,42 +4,30 @@ from utilidades.backblaze import Backblaze
 from general.models.documento import GenDocumento
 import requests
 import json
-
+import hashlib
 
 class DocumentoServicio():
 
     @staticmethod
     def generar_cufe(documento: GenDocumento):                                                   
-        cufe = 'Hola mundo'
-
-        '''$emisor = [
-            'numero_identificacion' => $arDocumento->getCuenta()->getNumeroIdentificacion(),
-            'nombre' => $arDocumento->getCuenta()->getNombre()
-        ];
-        $adquiriente = $documento['adquiriente'];
-        numero_completo = 
         
-        $datosCufe = [
-            'NumFac' => $numeroCompleto,
-            'FecFac' => $documento['fecha'],
-            'HorFac' => $documento['hora'],
-            'ValFac' => $documento['subtotal'],
-            'CodImp1' => "01",
-            'ValImp1' => $documento['total_iva'],
-            'CodImp2' => "04",
-            'ValImp2' => $documento['total_consumo'],
-            'CodImp3' => "03",
-            'ValImp3' => $documento['total_ica'],
-            'ValTotFac' => $documento['total_documento'],
-            'NitOFE' => $emisor['numero_identificacion'],
-            'NumAdq' => $adquiriente['numero_identificacion'],
-            'ClTec' => $claveTecnica,
-            'TipoAmbiente' => $documento['ambiente']
-        ];
-        $cufe = $datosCufe['NumFac'].$datosCufe['FecFac'].$datosCufe['HorFac'].$datosCufe['ValFac'].$datosCufe['CodImp1'].$datosCufe['ValImp1'].$datosCufe['CodImp2'].$datosCufe['ValImp2'].$datosCufe['CodImp3'].$datosCufe['ValImp3'].$datosCufe['ValTotFac'].$datosCufe['NitOFE'].$datosCufe['NumAdq'].$datosCufe['ClTec'].$datosCufe['TipoAmbiente'];
-        $UUID = hash('sha384', $cufe);
-        $QRCode = "NroFactura={$numeroCompleto} NitFacturador={$emisor['numero_identificacion']} NitAdquiriente={$adquiriente['numero_identificacion']} FechaFactura={$documento['fecha']} ValorTotalFactura={$documento['total_documento']} Cufe={$UUID} URL=https://catalogo-vpfe-hab.dian.gov.co/Document/FindDocument?documentKey={$UUID};partitionKey=co|06|94;emissionDate=20190620";
-        $softwareSecurityCode = hash('sha384',$softwareID."12345".$numeroCompleto);'''
+        emisor_numero_identificacion = '901192048'
+        adquiriente_numero_identificacion = documento.contacto.numero_identificacion
+        numero_completo = documento.numero
+        if documento.resolucion:
+            numero_completo = f'{documento.resolucion.prefijo}{documento.numero}'
 
+        
+        fecha_factura = ''
+        hora_factura = ''
+        subtotal = ''
+        iva = ''
+        consumo = ''
+        ica = ''
+        total = ''
+        clave_tecnica = ''
+        ambiente = '1'
+        cufe = f'{numero_completo}{fecha_factura}{hora_factura}{subtotal}01{iva}04{consumo}03{ica}{total}{emisor_numero_identificacion}{adquiriente_numero_identificacion}{clave_tecnica}{ambiente}'
+        UUID = hashlib.sha384(cufe.encode()).hexdigest()
 
-        return cufe                          
+        return UUID                          
