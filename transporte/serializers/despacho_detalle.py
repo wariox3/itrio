9df9d466b467cfe98dfea6fe1a02b43ae40c7903
wariox3 <maestradaz3@gmail.com>
2025-08-28 14:20:29 -0500

    feat: atributos despacho detalle

--- transporte/serializers/despacho_detalle.py
+++ transporte/serializers/despacho_detalle.py
@@ -2,11 +2,18 @@ from rest_framework import serializers
 from transporte.models.despacho_detalle import TteDespachoDetalle
 
 class TteDespachoDetalleSerializador(serializers.ModelSerializer):   
+    guia__fecha = serializers.CharField(source='guia.fecha', read_only=True, allow_null=True, default=None)
     guia__ciudad_destino__nombre = serializers.CharField(source='guia.ciudad_destino.nombre', read_only=True, allow_null=True, default=None)
-    guia__destinatario__nombre_corto = serializers.CharField(source='guia.destinatario.nombre_corto', read_only=True, allow_null=True, default=None)
+    guia__cliente__nombre_corto = serializers.CharField(source='guia.cliente.nombre_corto', read_only=True, allow_null=True, default=None)
+    guia__estado_entregado = serializers.BooleanField(source='guia.estado_entregado', read_only=True, allow_null=True, default=None)    
     class Meta:
         model = TteDespachoDetalle
         fields = ['id', 'fecha_registro', 'unidades', 'peso', 'volumen', 'peso_facturado', 'costo', 'declara', 
-                  'flete', 'manejo', 'recaudo', 'cobro_entrega', 'despacho', 'guia', 'guia__ciudad_destino__nombre', 'guia__destinatario__nombre_corto'] 
-        select_related_fields = ['guia']      
+                  'flete', 'manejo', 'recaudo', 'cobro_entrega', 'despacho', 
+                  'guia', 
+                  'guia__fecha',
+                  'guia__ciudad_destino__nombre', 
+                  'guia__cliente__nombre_corto',
+                  'guia__estado_entregado'] 
+        select_related_fields = ['guia', 'despacho', 'guia__ciudad_destino', 'guia__cliente']      
  
