5950761ad58cc9559ba8744241cad4bf06727b15
sebas957 <baxtian957@hotmail.com>
2025-08-29 17:23:42 -0500

    Ajustes serializador negocio

--- transporte/serializers/negocio.py
+++ transporte/serializers/negocio.py
@@ -5,15 +5,15 @@ class TteNegocioSerializador(serializers.ModelSerializer):
     contacto__nombre_corto = serializers.CharField(source='contacto.nombre_corto', read_only=True, allow_null=True, default=None)
     ciudad_origen__nombre = serializers.CharField(source='ciudad_origen.nombre', read_only=True, allow_null=True, default=None)
     ciudad_destino__nombre = serializers.CharField(source='ciudad_destino.nombre', read_only=True, allow_null=True, default=None)
+    servicio__nombre = serializers.CharField(source='servicio.nombre', read_only=True, allow_null=True, default=None)
+    operacion__nombre = serializers.CharField(source='operacion.nombre', read_only=True, allow_null=True, default=None)
+    producto__nombre = serializers.CharField(source='producto.nombre', read_only=True, allow_null=True, default=None)
+    empaque__nombre = serializers.CharField(source='producto.nombre', read_only=True, allow_null=True, default=None)
     class Meta:
         model = TteNegocio
         fields = ['id', 'fecha', 'fecha_registro', 'unidades' , 'peso', 'volumen', 'declara' ,'pago', 
                   'flete' , 'manejo', 'comentario', 'estado_aprobado', 'publicar','servicio', 'producto', 'empaque', 'operacion',
-                  'destinatario_nombre', 'destinatario_direccion', 'destinatario_telefono', 'destinatario_correo',
-                  'contacto',  
-                  'contacto__nombre_corto', 
-                  'ciudad_origen',
-                  'ciudad_origen__nombre' ,
-                  'ciudad_destino', 
-                  'ciudad_destino__nombre']
-        select_related_fields = ['contacto', 'ciudad_origen', 'ciudad_destino']  
+                  'destinatario_nombre', 'destinatario_direccion', 'destinatario_telefono', 'destinatario_correo', 'contacto',  'contacto__nombre_corto', 'ciudad_origen',
+                  'ciudad_origen__nombre' ,'ciudad_destino', 'ciudad_destino__nombre', 'servicio', 'servicio__nombre', 'operacion', 'operacion__nombre',
+                  'producto', 'producto__nombre', 'empaque', 'empaque__nombre']
+        select_related_fields = ['contacto', 'ciudad_origen', 'ciudad_destino', 'servicio', 'operacion', 'producto', 'empaque']  
