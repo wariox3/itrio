669af988fc316ed4d5daefff3976eedf6416bd33
sebas957 <baxtian957@hotmail.com>
2025-09-02 08:32:38 -0500

    feat: Se adicional filtros al modelo HumContrato

--- humano/filters/contrato.py
+++ humano/filters/contrato.py
@@ -2,9 +2,13 @@ import django_filters
 from humano.models.contrato import HumContrato
 
 class ContratoFilter(django_filters.FilterSet):
-    #contacto_numero_identificacion = django_filters.CharFilter(field_name='contacto__numero_identificacion', lookup_expr='icontains')
+    contacto__numero_identificacion = django_filters.CharFilter(field_name='contacto__numero_identificacion', lookup_expr='icontains')
+    contacto__nombre_corto = django_filters.CharFilter(field_name='contacto__nombre_corto', lookup_expr='icontains')
     class Meta:
         model = HumContrato        
         fields = {
             'id': ['exact', 'lte'],
+            'estado_terminado':['exact'],
+            'contacto__numero_identificacion': ['exact', 'icontains'],
+            'contacto__nombre_corto': ['exact','icontains'],
         }
