c747b68acc768776a37fa5c427b7c2a9feec339f
wariox3 <maestradaz3@gmail.com>
2025-09-04 10:18:38 -0500

    feat: Add estado_entregado and estado_facturado fields to GuiaFilter

--- transporte/filters/guia.py
+++ transporte/filters/guia.py
@@ -8,6 +8,8 @@ class GuiaFilter(django_filters.FilterSet):
         model = TteGuia
         fields = {'id': ['exact'],
                   'estado_despachado': ['exact'],
+                  'estado_entregado': ['exact'],
+                  'estado_facturado': ['exact'],
                   'ciudad_origen__nombre' : ['icontains'],
                   'ciudad_origen_id': ['exact'],
                   'ciudad_destino__nombre' : ['icontains'],
