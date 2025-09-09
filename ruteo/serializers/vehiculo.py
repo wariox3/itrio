04700184f039a7fb9c4af5876bde0eac12ca01cb
sebas957 <baxtian957@hotmail.com>
2025-09-09 11:56:07 -0500

    fix: AJustes veh

--- ruteo/serializers/vehiculo.py
+++ ruteo/serializers/vehiculo.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
 from ruteo.models.vehiculo import RutVehiculo
+from ruteo.models.franja import RutFranja
+
+class RutFranjaSimpleSerializador(serializers.ModelSerializer):
+    class Meta:
+        model = RutFranja
+        fields = ['codigo', 'nombre']  # Ajusta según los campos de tu modelo RutFranja
 
 class RutVehiculoSerializador(serializers.HyperlinkedModelSerializer):   
+    franjas = RutFranjaSimpleSerializador(many=True, read_only=True)
+    
     class Meta:
         model = RutVehiculo
         fields = [
             'id', 'placa', 'capacidad', 'tiempo', 
             'estado_activo', 'estado_asignado', 'usuario_app',
-            'prioridad'
+            'prioridad', 'franjas'
         ]
