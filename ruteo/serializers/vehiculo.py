3e2b6dfb469f6304aa6058e19239d01fb6976735
sebas957 <baxtian957@hotmail.com>
2025-09-09 11:52:32 -0500

    fix:ajuste vehi

--- ruteo/serializers/vehiculo.py
+++ ruteo/serializers/vehiculo.py
@@ -1,15 +1,11 @@
 from rest_framework import serializers
 from ruteo.models.vehiculo import RutVehiculo
 
-class RutVehiculoSerializador(serializers.ModelSerializer):
-    franjas_codigo = serializers.SerializerMethodField()
+class RutVehiculoSerializador(serializers.HyperlinkedModelSerializer):   
     class Meta:
         model = RutVehiculo
         fields = [
             'id', 'placa', 'capacidad', 'tiempo', 
             'estado_activo', 'estado_asignado', 'usuario_app',
-            'prioridad', 'franjas_codigo'
+            'prioridad'
         ]
-
-    def get_franjas_codigo(self, obj):
-        return list(obj.franjas.values_list('codigo', flat=True))
