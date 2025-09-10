69076a71dfbdd9c4a3fb6cc3596ba59c7cce9d3d
sebas957 <baxtian957@hotmail.com>
2025-09-10 16:16:36 -0500

    Feat: ajustes liquidacion adicional se agrega rel concepto

--- humano/models/liquidacion_adicional.py
+++ humano/models/liquidacion_adicional.py
@@ -1,11 +1,12 @@
 from django.db import models
 from humano.models.liquidacion import HumLiquidacion
-from humano.models.contrato import HumContrato
+from humano.models.concepto import HumConcepto
 from rest_framework import serializers
 
 class HumLiquidacionAdicional(models.Model):        
     adicional = models.DecimalField(max_digits=20, decimal_places=6, default=0)
     deduccion = models.DecimalField(max_digits=20, decimal_places=6, default=0)    
+    concepto = models.ForeignKey(HumConcepto, on_delete=models.PROTECT, related_name='liquidaciones_adicionales_concepto_rel')
     liquidacion = models.ForeignKey(HumLiquidacion, on_delete=models.PROTECT, related_name='liquidaciones_adicionales_liquidacion_rel')
 
     class Meta:
