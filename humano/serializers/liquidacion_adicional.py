df42555d2911ecd3fb8c8739c5eddb91bbcb0b34
sebas957 <baxtian957@hotmail.com>
2025-09-11 08:22:46 -0500

    feat: ajustes serializador

--- humano/serializers/liquidacion_adicional.py
+++ humano/serializers/liquidacion_adicional.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from humano.models.liquidacion_adicional import HumLiquidacionAdicional
 
-class HumLiquidacionAdicionalSerializador(serializers.HyperlinkedModelSerializer):
-
+class HumLiquidacionAdicionalSerializador(serializers.ModelSerializer):
+    concepto__nombre = serializers.CharField(source='concepto.nombre', read_only=True)
     class Meta:
         model = HumLiquidacionAdicional
-        fields = ['id', 'adicional', 'deduccion', 'liquidacion']
+        fields = ['id', 'adicional', 'deduccion', 'liquidacion', 'concepto', 'concepto__nombre']
+        select_related_fields = ['concepto']
 
