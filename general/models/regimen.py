1c3c72e8399e4ce7a025f914a65da99f3604ee2f
sebas957 <baxtian957@hotmail.com>
2025-09-11 11:48:27 -0500

    feat: ajustes inactivo regimen

--- general/models/regimen.py
+++ general/models/regimen.py
@@ -4,6 +4,7 @@ class GenRegimen(models.Model):
     id = models.BigIntegerField(primary_key=True)
     nombre = models.CharField(max_length=40)
     codigo_interface = models.CharField(max_length=2, null=True)
+    inactivo = models.BooleanField(default = False)
     
     class Meta:
         db_table = "gen_regimen"
--- general/serializers/regimen.py
+++ general/serializers/regimen.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class GenRegimenSerializador(serializers.ModelSerializer):
     class Meta:
         model = GenRegimen
-        fields = ['id', 'nombre']  
+        fields = ['id', 'nombre', 'inactivo']  
