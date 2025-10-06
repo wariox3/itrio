c52d355ab9230389c307d34b548a2635e11830b7
wariox3 <maestradaz3@gmail.com>
2025-10-06 15:25:33 -0500

    feat: adicionar filtros

--- general/filters/documento_detalle.py
+++ general/filters/documento_detalle.py
@@ -5,11 +5,14 @@ from django_filters import NumberFilter
 class DocumentoDetalleFilter(django_filters.FilterSet):
     item__nombre = django_filters.CharFilter(field_name='item__nombre', lookup_expr='icontains')     
     documento__contacto_id = django_filters.CharFilter(field_name='documento__contacto_id', lookup_expr='exact')
+    documento__contrato_id = django_filters.CharFilter(field_name='documento__contrato_id', lookup_expr='exact')
+    documento__fecha = django_filters.DateFilter(field_name='documento__fecha')
     documento__estado_aprobado = django_filters.BooleanFilter(field_name='documento__estado_aprobado', lookup_expr='exact')    
     documento__contacto__nombre_corto = django_filters.CharFilter(field_name='documento__contacto__nombre_corto', lookup_expr='icontains')    
     documento__contacto__numero_identificacion = django_filters.CharFilter(field_name='documento__contacto__numero_identificacion', lookup_expr='icontains')    
     documento__programacion_detalle__programacion_id = django_filters.NumberFilter(field_name='documento__programacion_detalle__programacion_id')
     documento__documento_tipo_id = django_filters.NumberFilter(field_name='documento__documento_tipo_id')
+    documento__documento_tipo__documento_clase_id = django_filters.NumberFilter(field_name='documento__documento_tipo__documento_clase_id')
     
     inventario = django_filters.BooleanFilter(
         method='filtrar_inventario',
@@ -40,10 +43,12 @@ class DocumentoDetalleFilter(django_filters.FilterSet):
                     'documento_afectado_id': ['exact'],
                     'item__nombre':['icontains'],
                     'documento__estado_aprobado':['exact'],
+                    'documento__fecha':['lte', 'gte', 'exact'],
                     'documento__numero':['exact', 'icontains'],
                     'documento__contacto_id':['exact'],
                     'documento__contacto__nombre_corto':['exact', 'icontains'],
                     'documento__contacto__numero_identificacion':['exact', 'icontains'],
                     'documento__documento_tipo__venta':['exact'],                                                            
-                    'documento__programacion_detalle__programacion_id': ['exact'],                                        
+                    'documento__programacion_detalle__programacion_id': ['exact'],  
+                    'documento__documento_tipo__documento_clase_id': ['exact'],                                     
                 }
