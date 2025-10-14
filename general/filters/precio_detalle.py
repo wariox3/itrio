e49769dcf58c801bd16272ae1c86c273ee966ff8
sebas957 <baxtian957@hotmail.com>
2025-10-14 10:11:42 -0500

    feat: Precio_detalle

---
+++ general/filters/precio_detalle.py
@@ -0,0 +1,8 @@
+import django_filters
+from general.models.precio_detalle import GenPrecioDetalle
+
+class PrecioDetalleFilter(django_filters.FilterSet):    
+    class Meta:
+        model = GenPrecioDetalle        
+        fields = {'id': ['exact'],
+                  'precio': ['exact']}
--- general/views/precio_detalle.py
+++ general/views/precio_detalle.py
@@ -1,8 +1,38 @@
 from rest_framework import viewsets, permissions
 from general.models.precio_detalle import GenPrecioDetalle
 from general.serializers.precio_detalle import GenPrecioDetalleSerializador
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from general.filters.precio_detalle import PrecioDetalleFilter
 
 class PrecioDetalleViewSet(viewsets.ModelViewSet):
     queryset = GenPrecioDetalle.objects.all()
     serializer_class = GenPrecioDetalleSerializador
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]    
+    filterset_class = PrecioDetalleFilter 
+    serializadores = {
+        'lista': GenPrecioDetalleSerializador
+    }
+
+    def get_serializer_class(self):
+        serializador_parametro = self.request.query_params.get('serializador', None)
+        if not serializador_parametro or serializador_parametro not in self.serializadores:
+            return GenPrecioDetalleSerializador
+        return self.serializadores[serializador_parametro]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        serializer_class = self.get_serializer_class()        
+        select_related = getattr(serializer_class.Meta, 'select_related_fields', [])
+        if select_related:
+            queryset = queryset.select_related(*select_related)        
+        campos = serializer_class.Meta.fields        
+        if campos and campos != '__all__':
+            queryset = queryset.only(*campos) 
+        return queryset 
+
+    def list(self, request, *args, **kwargs):
+        if request.query_params.get('lista', '').lower() == 'true':
+            self.pagination_class = None
+        return super().list(request, *args, **kwargs)    
