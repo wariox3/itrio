1775448daa2b19cc46987fe7525745eba9da180d
wariox3 <maestradaz3@gmail.com>
2025-10-15 16:56:50 -0500

    feat: estandarizar serializador

--- general/serializers/configuracion.py
+++ general/serializers/configuracion.py
@@ -6,10 +6,7 @@ from humano.models.entidad import HumEntidad
 from rest_framework import serializers
 from decouple import config
 
-class GenConfiguracionSerializador(serializers.HyperlinkedModelSerializer):
-    empresa = serializers.PrimaryKeyRelatedField(queryset=GenEmpresa.objects.all())
-    hum_entidad_riesgo = serializers.PrimaryKeyRelatedField(queryset=HumEntidad.objects.all(), default=None, allow_null=True)
-    pos_documento_tipo = serializers.PrimaryKeyRelatedField(queryset=GenDocumentoTipo.objects.all(), default=None, allow_null=True)
+class GenConfiguracionSerializador(serializers.ModelSerializer):
     class Meta:
         model = GenConfiguracion
         fields = ['id', 'empresa', 'informacion_factura', 'informacion_factura_superior', 
@@ -28,7 +25,8 @@ class GenConfiguracionSerializador(serializers.HyperlinkedModelSerializer):
             'rut_direccion_origen', 
             'rut_latitud', 
             'rut_longitud'
-        ]      
+        ]   
+        select_related_fields = ['empresa', 'hum_entidad_riesgo', 'pos_documento_tipo']   
 
 class GenConfiguracionRndcSerializador(serializers.ModelSerializer):
     empresa__numero_identificacion = serializers.CharField(source='empresa.numero_identificacion', read_only=True)
