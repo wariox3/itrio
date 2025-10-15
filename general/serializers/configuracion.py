29d81d7eb9aee478f7df44d06cf0f929eacb07a4
wariox3 <maestradaz3@gmail.com>
2025-10-15 17:14:49 -0500

    feat: Add related item names to GenConfiguracion serializer and update select_related_fields

--- general/serializers/configuracion.py
+++ general/serializers/configuracion.py
@@ -7,14 +7,20 @@ from rest_framework import serializers
 from decouple import config
 
 class GenConfiguracionSerializador(serializers.ModelSerializer):
+    gen_item_administracion__nombre = serializers.CharField(source='gen_item_administracion.nombre', read_only=True)
+    gen_item_imprevisto__nombre = serializers.CharField(source='gen_item_imprevisto.nombre', read_only=True)
+    gen_item_utilidad__nombre = serializers.CharField(source='gen_item_utilidad.nombre', read_only=True)
     class Meta:
         model = GenConfiguracion
         fields = ['id', 'empresa', 'informacion_factura', 'informacion_factura_superior', 
             'gen_uvt', 
             'gen_emitir_automaticamente',
             'gen_item_administracion',
+            'gen_item_administracion__nombre',
             'gen_item_imprevisto',
+            'gen_item_imprevisto__nombre',
             'gen_item_utilidad',
+            'gen_item_utilidad__nombre',
             'hum_factor', 
             'hum_salario_minimo', 
             'hum_auxilio_transporte', 
@@ -26,7 +32,7 @@ class GenConfiguracionSerializador(serializers.ModelSerializer):
             'rut_latitud', 
             'rut_longitud'
         ]   
-        select_related_fields = ['empresa', 'hum_entidad_riesgo', 'pos_documento_tipo']   
+        select_related_fields = ['empresa', 'hum_entidad_riesgo', 'pos_documento_tipo', 'gen_item_imprevisto', 'gen_item_administracion', 'gen_item_utilidad']   
 
 class GenConfiguracionRndcSerializador(serializers.ModelSerializer):
     empresa__numero_identificacion = serializers.CharField(source='empresa.numero_identificacion', read_only=True)
