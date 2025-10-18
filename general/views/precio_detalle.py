630f4220c96152e68af33250c93f4ddb0ab6e0e4
wariox3 <maestradaz3@gmail.com>
2025-10-18 10:43:00 -0500

    feat: funcionalidad para importar listas de precios

--- general/views/precio_detalle.py
+++ general/views/precio_detalle.py
@@ -1,11 +1,16 @@
 from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from general.models.precio_detalle import GenPrecioDetalle
+from general.models.item import GenItem
 from general.serializers.precio_detalle import GenPrecioDetalleSerializador
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from general.filters.precio_detalle import PrecioDetalleFilter
 from rest_framework.response import Response
+import base64
+import openpyxl
+from io import BytesIO
+import gc
 
 class PrecioDetalleViewSet(viewsets.ModelViewSet):
     queryset = GenPrecioDetalle.objects.all()
@@ -39,7 +44,6 @@ class PrecioDetalleViewSet(viewsets.ModelViewSet):
             self.pagination_class = None
         return super().list(request, *args, **kwargs)    
     
-
     @action(detail=False, methods=["get"], url_path=r'consultar_precio')
     def consultar_precio(self, request):
         precio_id = request.query_params.get('precio_id', None)
@@ -58,3 +62,79 @@ class PrecioDetalleViewSet(viewsets.ModelViewSet):
         else:
             return Response({'mensaje':'Faltan parametros', 'codigo':1}, status=status.HTTP_400_BAD_REQUEST)
                 
+    @action(detail=False, methods=["post"], url_path=r'importar',)
+    def importar(self, request):
+        raw = request.data        
+        precio_id = raw.get('precio_id')
+        archivo_base64 = raw.get('archivo_base64')
+        if precio_id and archivo_base64:
+            try:
+                archivo_data = base64.b64decode(archivo_base64)
+                archivo = BytesIO(archivo_data)
+                wb = openpyxl.load_workbook(archivo)
+                sheet = wb.active    
+            except Exception as e:     
+                return Response({'mensaje': 'Error procesando el archivo, valide que es un archivo de excel .xlsx', 'codigo': 15}, status=status.HTTP_400_BAD_REQUEST)  
+                        
+            #itemes_map = {int(i.id): i for i in GenItem.objects.all()}
+            itemes_map = set(GenItem.objects.values_list('id', flat=True))
+            data_modelo = []
+            errores = False
+            errores_datos = []
+            registros_importados = 0
+            
+            for i, row in enumerate(sheet.iter_rows(min_row=2, values_only=True), start=2):
+                data = {
+                    'precio': precio_id,
+                    'item': row[0],                    
+                    'vr_precio': row[1]                    
+                }
+
+                if data['vr_precio'] is not None:
+                    try:
+                        precio_str = str(data['vr_precio'])
+                        if '.' in precio_str:
+                            decimal_part = precio_str.split('.')[1]
+                            if len(decimal_part) > 2:
+                                errores = True
+                                error_dato = {
+                                    'fila': i,
+                                    'errores': {'precio': ['No puede tener más de 2 decimales']}
+                                }                                    
+                                errores_datos.append(error_dato)
+                                continue
+                    except (ValueError, TypeError):
+                        errores = True
+                        error_dato = {
+                            'fila': i,
+                            'errores': {'precio': ['Formato inválido']}
+                        }                                    
+                        errores_datos.append(error_dato)
+                        continue                
+   
+                if data['item'] not in itemes_map:
+                    data['item'] = None                               
+                serializer = GenPrecioDetalleSerializador(data=data)
+                if serializer.is_valid():
+                    validated_data = serializer.validated_data
+                    data_modelo.append((validated_data))
+                else:
+                    errores = True
+                    error_dato = {
+                        'fila': i,
+                        'errores': serializer.errors
+                    }                                    
+                    errores_datos.append(error_dato)   
+
+            if not errores:
+                for detalle in data_modelo:
+                    GenPrecioDetalle.objects.create(**detalle)
+                    registros_importados += 1
+
+                gc.collect()
+                return Response({'registros_importados': registros_importados}, status=status.HTTP_200_OK)
+            else:
+                gc.collect()                    
+                return Response({'mensaje': 'Errores de validacion', 'codigo': 1, 'errores_validador': errores_datos}, status=status.HTTP_400_BAD_REQUEST)       
+        else:
+            return Response({'mensaje': 'Faltan parametros', 'codigo': 1}, status=status.HTTP_400_BAD_REQUEST)                
