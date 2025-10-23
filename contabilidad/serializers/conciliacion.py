7367a11cbb8f7b758ec5f1187dd658291321df3e
sebas957 <baxtian957@hotmail.com>
2025-10-23 16:03:07 -0500

    feat: Cuenta conciliacion

--- contabilidad/serializers/conciliacion.py
+++ contabilidad/serializers/conciliacion.py
@@ -4,11 +4,13 @@ from contabilidad.models.conciliacion import ConConciliacion
 class ConConciliacionSerializador(serializers.ModelSerializer): 
     cuenta_banco__nombre = serializers.CharField(source='cuenta_banco.nombre', read_only=True)   
     cuenta_banco__cuenta__codigo = serializers.CharField(source='cuenta_banco.cuenta.codigo', read_only=True)   
+    cuenta_banco__cuenta__nombre = serializers.CharField(source='cuenta_banco.cuenta.nombre', read_only=True)   
     class Meta:
         model = ConConciliacion
         fields = ['id', 'fecha_desde', 'fecha_hasta', 
                   'cuenta_banco',
                   'cuenta_banco__nombre',
-                  'cuenta_banco__cuenta__codigo']  
+                  'cuenta_banco__cuenta__codigo',
+                  'cuenta_banco__cuenta__nombre']  
         select_related_fields = ['cuenta_banco']              
         
