3b1e036175334c85f702d465c8ccc2c5571f7649
sebas957 <baxtian957@hotmail.com>
2025-10-23 15:56:16 -0500

    Feat: FIlter conciliacion soporte

--- contabilidad/filters/conciliacion_soporte.py
+++ contabilidad/filters/conciliacion_soporte.py
@@ -4,4 +4,5 @@ from contabilidad.models.conciliacion_soporte import ConConciliacionSoporte
 class ConciliacionSoporteFilter(django_filters.FilterSet):    
     class Meta:
         model = ConConciliacionSoporte
-        fields = {'id': ['exact']}
+        fields = {'id': ['exact'],
+                  'conciliacion_id': ['exact']}
