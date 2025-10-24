087413a10c544f0bfa3322042e90898fe4d7c096
sebas957 <baxtian957@hotmail.com>
2025-10-24 08:36:31 -0500

    Feat: FIltro conciliacion_detalle

--- contabilidad/filters/conciliacion_detalle.py
+++ contabilidad/filters/conciliacion_detalle.py
@@ -4,4 +4,5 @@ from contabilidad.models.conciliacion_detalle import ConConciliacionDetalle
 class ConciliacionDetalleFilter(django_filters.FilterSet):    
     class Meta:
         model = ConConciliacionDetalle
-        fields = {'id': ['exact']}
+        fields = {'id': ['exact'],
+                'conciliacion_id': ['exact']}
