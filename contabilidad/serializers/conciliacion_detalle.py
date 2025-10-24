5b144f1b62d9b1d5d518ae46cfd27383003276bf
sebas957 <baxtian957@hotmail.com>
2025-10-24 08:47:05 -0500

    feat: Ajustes serialiazdor conciliacion_detalle

--- contabilidad/serializers/conciliacion_detalle.py
+++ contabilidad/serializers/conciliacion_detalle.py
@@ -2,12 +2,18 @@ from rest_framework import serializers
 from contabilidad.models.conciliacion_detalle import ConConciliacionDetalle
 
 class ConConciliacionDetalleSerializador(serializers.ModelSerializer):     
+    documento__numero = serializers.CharField(source='documento.numero', read_only=True)   
+    documento__documento_tipo__nombre = serializers.CharField(source='documento.documento_tipo.nombre', read_only=True)   
+    cuenta__codigo = serializers.CharField(source='cuenta.codigo', read_only=True)   
     class Meta:
         model = ConConciliacionDetalle
         fields = ['id', 'fecha', 'debito', 'credito', 'detalle', 'estado_conciliado', 
                   'conciliacion', 
-                  'cuenta', 
+                  'cuenta',
+                  'cuenta__codigo', 
                   'contacto', 
-                  'documento']  
+                  'documento',
+                  'documento__numero',
+                  'documento__documento_tipo__nombre']  
         select_related_fields = ['conciliacion', 'cuenta', 'contacto', 'documento']              
         
