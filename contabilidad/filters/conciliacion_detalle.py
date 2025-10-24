6575e1e5c9d0a32ee1fac2fb898fa66d645719b3
sebas957 <baxtian957@hotmail.com>
2025-10-24 15:09:51 -0500

    feat: Filtro por estado conciliado

--- contabilidad/filters/conciliacion_detalle.py
+++ contabilidad/filters/conciliacion_detalle.py
@@ -5,4 +5,5 @@ class ConciliacionDetalleFilter(django_filters.FilterSet):
     class Meta:
         model = ConConciliacionDetalle
         fields = {'id': ['exact'],
-                'conciliacion_id': ['exact']}
+                'conciliacion_id': ['exact'],
+                'estado_conciliado': ['exact']}
--- contabilidad/filters/conciliacion_soporte.py
+++ contabilidad/filters/conciliacion_soporte.py
@@ -5,4 +5,5 @@ class ConciliacionSoporteFilter(django_filters.FilterSet):
     class Meta:
         model = ConConciliacionSoporte
         fields = {'id': ['exact'],
-                  'conciliacion_id': ['exact']}
+                  'conciliacion_id': ['exact'],
+                  'estado_conciliado': ['exact']}
