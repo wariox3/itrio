3ce883dfa272b3b3c95e818b1679184fabf1206c
wariox3 <maestradaz3@gmail.com>
2025-11-07 15:57:27 -0500

    feat: update SIMPLE_JWT settings for access and refresh token lifetimes

--- itrioapp/settings.py
+++ itrioapp/settings.py
@@ -182,14 +182,25 @@ REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'itrioapp.exceptions.custom_exception_handler',
 }
 
+# actual
 # SIMPLE_JWT = {      
 #     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
 #     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),    
 # }
 
+# propuesta
+#SIMPLE_JWT = {      
+#    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+#    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+#    'ROTATE_REFRESH_TOKENS': True,
+#    'BLACKLIST_AFTER_ROTATION': True,
+#    'UPDATE_LAST_LOGIN': True,    
+#}
+
+# desarrollo
 SIMPLE_JWT = {      
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': True,    
