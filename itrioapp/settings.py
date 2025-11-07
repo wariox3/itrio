6886c60341e66ffb4616685445d8ea539cd5a68f
wariox3 <maestradaz3@gmail.com>
2025-11-07 15:55:20 -0500

    feat: update JWT token lifetimes and enable rotation and blacklist features

--- itrioapp/settings.py
+++ itrioapp/settings.py
@@ -188,8 +188,11 @@ REST_FRAMEWORK = {
 # }
 
 SIMPLE_JWT = {      
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=2),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': True,    
 }
 
 AUTH_USER_MODEL = 'seguridad.User'
