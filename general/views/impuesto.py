5cc5b677102fc2d7fc11b71fdbb27cfb245d32aa
sebas957 <baxtian957@hotmail.com>
2026-01-09 14:14:02 -0500

    feat: Se ajusta la cantidad de registros en el get general de impuestos

--- general/views/impuesto.py
+++ general/views/impuesto.py
@@ -3,20 +3,30 @@ from rest_framework.decorators import action
 from general.models.impuesto import GenImpuesto
 from general.serializers.impuesto import GenImpuestoSerializador, GenImpuestoSeleccionarSerializador
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 
 class ImpuestoViewSet(viewsets.ModelViewSet):
     queryset = GenImpuesto.objects.all()
     serializer_class = GenImpuestoSerializador
     permission_classes = [permissions.IsAuthenticated]  
 
+    def get_serializer_class(self):
+        serializador_parametro = self.request.query_params.get('serializador', None)
+        if not serializador_parametro or serializador_parametro not in self.serializadores:
+            return GenImpuestoSerializador
+        return self.serializadores[serializador_parametro]
+
     def get_queryset(self):
-        queryset = super().get_queryset()               
-        select_related = getattr(self.serializer_class.Meta, 'select_related_fields', [])
+        self.pagination_class = PageNumberPagination
+        self.pagination_class.page_size = 100
+        queryset = super().get_queryset()
+        serializer_class = self.get_serializer_class()        
+        select_related = getattr(serializer_class.Meta, 'select_related_fields', [])
         if select_related:
             queryset = queryset.select_related(*select_related)        
-        campos = self.serializer_class.Meta.fields        
+        campos = serializer_class.Meta.fields        
         if campos and campos != '__all__':
-            queryset = queryset.only(*campos)     
+            queryset = queryset.only(*campos) 
         return queryset 
 
     @action(detail=False, methods=["get"], url_path=r'seleccionar')
