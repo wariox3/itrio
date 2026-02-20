efde91b82a902699b55de8b18f8a660b73a1dca0
sebas957 <baxtian957@hotmail.com>
2026-02-20 13:40:59 -0500

    feat: Filtro noveddad tipo

--- humano/filters/novedad.py
+++ humano/filters/novedad.py
@@ -2,8 +2,10 @@ import django_filters
 from humano.models.novedad import HumNovedad
 
 class NovedadFilter(django_filters.FilterSet):
+    novedad_tipo__nombre = django_filters.CharFilter(field_name='novedad_tipo__nombre', lookup_expr='icontains')
     class Meta:
         model = HumNovedad        
         fields = {
             'id': ['exact', 'lte'],
+            'novedad_tipo__nombre': ['exact', 'icontains'],
         }
