cd5e4c46a264d9eca46b6c209cccc7a1048df9fd
sebas957 <baxtian957@hotmail.com>
2026-06-02 15:04:52 -0500

    Feat: Se ajustan filtros para administrador de novedades

--- humano/filters/novedad.py
+++ humano/filters/novedad.py
@@ -3,9 +3,15 @@ from humano.models.novedad import HumNovedad
 
 class NovedadFilter(django_filters.FilterSet):
     novedad_tipo__nombre = django_filters.CharFilter(field_name='novedad_tipo__nombre', lookup_expr='icontains')
+    contrato__contacto__numero_identificacion = django_filters.CharFilter(field_name='contrato__contacto__numero_identificacion', lookup_expr='icontains')
+    contrato__contacto__nombre_corto = django_filters.CharFilter(field_name='contrato__contacto__nombre_corto', lookup_expr='icontains')
     class Meta:
         model = HumNovedad        
         fields = {
             'id': ['exact', 'lte'],
             'novedad_tipo__nombre': ['exact', 'icontains'],
+            'contrato__contacto__numero_identificacion': ['exact', 'icontains'],
+            'contrato__contacto__nombre_corto': ['exact', 'icontains'],
+            'fecha_desde': ['gte', 'lte', 'gt', 'lt', 'exact'], 
+            'fecha_hasta': ['gte', 'lte', 'gt', 'lt', 'exact'], 
         }
